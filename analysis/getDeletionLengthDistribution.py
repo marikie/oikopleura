4c0e726a5c83b6e49945974d24f4e7e4c4c5737d
mrknkgw <mariko.n.0424@gmail.com>
2022-05-06 08:34:25 +0000

    modified to directly take an alignment file instead of a pickle file

--- analysis/getDeletionLengthDistribution.py
+++ analysis/getDeletionLengthDistribution.py
@@ -2,29 +2,32 @@
 Input: a pickled dictionary (key: readID, value: a list of Alignment objects)
 Output: a column of numbers (deletion length distribution)
 '''
-import pickle
+from getAlignmentObjs import getMultiMAFEntries
 import argparse
 import re
 
-def main(pickleFile):
-    with open(pickleFile, 'rb') as f:
-        readID_alignments_dict = pickle.load(f)
-        for alignments in readID_alignments_dict.values():
-            for aln in alignments:
-                cigarListIter = iter(re.split(r'(\D)', aln.cigar))
-                for num, letter in zip(cigarListIter,cigarListIter):
-                    if letter=='D': print(num)
-                    else: pass
+
+def main(alignmentFile):
+    for readID, alignments in getMultiMAFEntries(alignmentFile):
+        for aln in alignments:
+            cigarListIter = iter(re.split(r'(\D)', aln.cigar))
+            for num, letter in zip(cigarListIter, cigarListIter):
+                if letter == 'D':
+                    print(num)
+                else:
+                    pass
+
 
 if __name__ == '__main__':
     '''
     File Parsing
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('pickleFile', help='a picke file: a dictionary, key: readID, value: a list\
-                        of Alignment objects')
+    parser.add_argument('alignmentFile',
+                        help='spliced alignments of reads to\
+                              reference in MAF format')
     args = parser.parse_args()
     '''
     MAIN
     '''
-    main(args.pickleFile)
+    main(args.alignmentFile)
