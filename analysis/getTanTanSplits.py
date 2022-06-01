5c69baef0b216e3c7cc202ab332c57ba499b0ae0
mrknkgw <mariko.n.0424@gmail.com>
2022-06-01 07:17:59 +0000

    added getTanTanSplits.py

---
+++ analysis/getTanTanSplits.py
@@ -0,0 +1,91 @@
+'''
+Input:
+    - an output file of "tantan  -f 4" (.tsv file)
+    - an alignment file (.maf file)
+Output:
+    print out spliced region with tandem repeats
+    on both exon end and exon start
+    - maf entry
+    - tantan row
+    - tantan row
+'''
+import argparse
+import csv
+from getAlignmentObjs import getMultiMAFEntries
+
+
+def getTandemRepeatData(tantanFile):
+    with open(tantanFile, 'r') as f:
+        print("--- Reading tantanFile")
+        tandemRepeatData = [row for row in csv.reader(f, delimiter='\t')]
+    # sort according to chromosom name, start position, and end position
+        print("--- Sorting tandemRepeatData")
+    tandemRepeatData_sorted = sorted(tandemRepeatData,
+                                     key=lambda x: (x[0], x[1], x[2]))
+    return tandemRepeatData_sorted
+
+
+def isOverlapped(trRow, aln, EndOrBegin):
+    if EndOrBegin == 'End':
+        if (trRow[0], trRow[1]) < (aln.gChr, aln.gEnd) and (aln.gChr, aln.gEnd) <= (trRow[0], trRow[2]):
+            return True
+        else:
+            return False
+    else:  # 'Begin'
+        if (trRow[0], trRow[1]) < (aln.gChr, aln.gStart) and (aln.gChr, aln.gStart) <= (trRow[0], trRow[2]):
+            return True
+        else:
+            return False
+
+
+def getTan(trData, aln, EndOrBegin):
+    if EndOrBegin == 'End':
+        for trRow in trData:
+            if isOverlapped(trRow, aln, 'End'):
+                return trRow
+        else:
+            return None
+    else:  # 'Begin'
+        for trRow in trData:
+            if isOverlapped(trRow, aln, 'Begin'):
+                return trRow
+        else:
+            return None
+
+
+def printTantanSplits(trData, alignmentFile):
+    print("--- Searching tan-tan-splits")
+    for readID, alignments in getMultiMAFEntries(alignmentFile):
+        # sort alignments
+        alignments_sorted = sorted(alignments,
+                                   key=lambda a: (a.gChr, a.gStart, a.gEnd))
+        for aln1, aln2 in zip(alignments_sorted, alignments_sorted[1:]):
+            tan1 = getTan(trData, aln1, 'End')
+            tan2 = getTan(trData, aln2, 'Begin')
+            if tan1 and tan2:
+                print(aln1)
+                print(aln2)
+                print('\t'.join(tan1))
+                print('\t'.join(tan2))
+
+
+if __name__ == '__main__':
+    '''
+    File Parsing
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tantanFile',
+                        help='an output .tsv file of \
+                        \"tantan -f 4\"')
+    parser.add_argument('alignmentFile',
+                        help='spliced alignments of reads \
+                        to reference in MAF format')
+    args = parser.parse_args()
+    '''
+    Read the tantan file
+    '''
+    trData = getTandemRepeatData(args.tantanFile)
+    '''
+    Get tantan splits
+    '''
+    printTantanSplits(trData, args.alignmentFile)
