b3a1cd2b65758870ff131b16cabd3201ff101501
mrknkgw <mariko.n.0424@gmail.com>
2022-06-17 09:14:56 +0000

    bug fixed: consider strand properly

--- analysis/countCanonicalNonCanonicalSS.py
+++ analysis/countCanonicalNonCanonicalSS.py
@@ -8,53 +8,58 @@ Input: an alignment file (MAF format)
 Output: counts of canonical and non-canonical splicing signals
 '''
 import argparse
-from getAlignmentObjs import getMultiMAFEntries
-from getSplicingSignalsDistOfExactSplicings import convert2CoorOnOppositeStrand
+from Util import getMultiMAFEntries
+from Util import convert2CoorOnOppositeStrand
+from Util import getIntronCoord
 import sys
 
 
 def main(alignmentFile):
     intronCoords_SS_dict = {}
 
-    print('Getting all introns...')
+    print('Putting introns into a dict...')
     for readID, alignments in getMultiMAFEntries(alignmentFile):
-        # pre-sorting...
-        # adjust all coordinates to + strand's coordinates
-        for aln in alignments:
-            if aln.rStrand == '+':
-                pass
-            else:
-                aln.rStart, aln.rEnd = convert2CoorOnOppositeStrand(aln)
-        # sort according to read's start position
-        alignments.sort(key=lambda aln: aln.rStart)
+        # prerequisite:
+        # alignments are already sorted
+        # according to + strand's coordinates
 
-        # determining the order of alignments...
+        readStrand = None
+        # get the order of alignments
         # if the first alignment has donor and doesn't have acceptor
         # or the last alignment doesn't have donor and has acceptor
         if (alignments[0].don and not alignments[0].acc)\
                 or (not alignments[-1].don and alignments[-1].acc):
-            # do nothing
-            pass
+            # set readStrand to '+'
+            readStrand = '+'
+            # adjust all coordinates to + strand's coordinates
+            for aln in alignments:
+                if aln.rStrand == '-':
+                    aln.rStart, aln.rEnd = convert2CoorOnOppositeStrand(aln)
         # if the last alignment has donor and doesn't have acceptor
         # or the first alignment doesn't have donor and has acceptor
         elif (alignments[-1].don and not alignments[-1].acc)\
                 or (not alignments[0].don and alignments[0].acc):
+            # set readStrand to '-'
+            readStrand = '-'
             # reverse the alignments list
-            alignments.sort(reverse=True, key=lambda aln: aln.rStart)
+            alignments.reverse()
             # adjust all coordinates to - strand's coordinates
             for aln in alignments:
-                aln.rStart, aln.rEnd = convert2CoorOnOppositeStrand(aln)
+                if aln.rStrand == '+':
+                    aln.rStart, aln.rEnd = convert2CoorOnOppositeStrand(aln)
         else:
             # go to next readID
+            # (do NOT append to alignments_list)
             continue
 
         for aln1, aln2 in zip(alignments, alignments[1:]):
-            # if two separate alignments are continuous on the read
+            # if two separate alignments are continuous on the reaad
+            # (checking only "Exact Splits")
+            # do NOT append alignments with inexact splits
             if aln2.rStart - aln1.rEnd == 0:
-                # print('exact split')
-                # add the intron to the intronCoords_SS_dict
-                intronCoords = ((aln1.gChr, aln1.gEnd+1),
-                                (aln2.gChr, aln2.gStart))
+                intronStart, intronEnd = getIntronCoord(readStrand, aln1, aln2)
+                # add to dict
+                intronCoords = (intronStart, intronEnd)
                 ss = (aln1.don, aln2.acc)
                 if intronCoords not in intronCoords_SS_dict:
                     intronCoords_SS_dict[intronCoords] = [ss]
@@ -64,26 +69,20 @@ def main(alignmentFile):
     print('counting canonical and non-canonical introns...')
     canonical = 0
     non_canonical = 0
-    intronCoords_set = set()
     for intronCoords, sslist in intronCoords_SS_dict.items():
-        # print('{} {}'.format(intronCoords, sslist))
-        if intronCoords in intronCoords_set:
-            # go to next intron
-            continue
-        else:
-            # add intronCoords to intronCoords_set
-            intronCoords_set.add(intronCoords)
-            # count intron which has only 1 type of splicing signal
-            if len(set(sslist)) == 1:
-                # canonical splicing signal
-                if sslist[0] == ('GT', 'AG') or sslist[0] == ('CT', 'AC'):
-                    canonical += 1
-                # non-canonical splicing signal
-                else:
-                    non_canonical += 1
-            # introns which have >= 2 types of splicing signals
+        sslist_upper = list(map(lambda t: (t[0].upper(), t[1].upper()),
+                                sslist))
+        # count intron which has only 1 type of splicing signal
+        if len(set(sslist_upper)) == 1:
+            # canonical splicing signal
+            if sslist_upper[0] == ('GT', 'AG') or sslist[0] == ('CT', 'AC'):
+                canonical += 1
+            # non-canonical splicing signal
             else:
-                print('{} {}'.format(intronCoords, sslist), file=sys.stderr)
+                non_canonical += 1
+        # introns which have >= 2 types of splicing signals
+        else:
+            print('{} {}'.format(intronCoords, sslist), file=sys.stderr)
 
     # print the result
     print('\n\n')
