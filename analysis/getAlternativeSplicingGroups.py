85a015cfcc72204e4f40a97accd0cceb31b3ac98
mrknkgw <mariko.n.0424@gmail.com>
2022-11-15 09:17:00 +0000

    updated the code of getAlternativeSplicingGroups.py
    getting complicated. need to test every function.

--- analysis/getAlternativeSplicingGroups.py
+++ analysis/getAlternativeSplicingGroups.py
@@ -29,6 +29,16 @@ Input:
 Output:
     - a .json file
         {'intronCoord': {
+            'intronStart': {
+                'chr': str,
+                'pos': int,
+                'strand': str
+            },
+            'intronEnd':{
+                'chr': str,
+                'pos': int,
+                'strand': str
+            },
             'intronLength': int,
             'splicingSignal': ['GT', 'AG'],
             'numOfReads': int,
@@ -49,7 +59,6 @@ Output:
                 'numOfReads': int,
                 'readIDs': [...]
                 }
-            'readIDs': [...]
             },
             ...
         }
@@ -60,46 +69,45 @@ from Util import getMultiMAFEntries_all
 from Util import getIntronCoord
 
 
-def getIntronList(intronJsonFile):
+def getIntronDict(intronJsonFile):
     print('--- Reading intronJsonFile')
     # load the intronJsonFile
     with open(intronJsonFile, 'r') as f:
         intron_dict = json.load(f)
 
-    print('---Sorting intron_list')
-    # make a list of values
-    # and sort
-    intron_list = sorted(list(intron_dict.values()),
-                         key=lambda x:
-                         ((x['intronStart']['chr'], x['intronStart']['pos']),
-                           x['intronStart']['strand']),
-                          (x['intronEnd']['chr'], x['intronEnd']['pos'],
-                           x['intronEnd']['strand']))
-    return intron_list
+    print('--- Organizing intron_dict')
+    for intronCoord, intronInfo in intron_dict.items():
+        # add 'numOfReads'
+        intron_dict[intronCoord]['numOfReads'] = len(intronInfo['readIDs'])
+        # delete 'readIDs' and 'alignments'
+        del intron_dict[intronCoord]['readIDs']
+        del intron_dict[intronCoord]['alignments']
+
+    print('--- Sorting intron_dict')
+    # sort by intron end coord
+    dict(sorted(intron_dict.items(),
+         key=lambda x:
+         (x[1]['intronEnd']['chr'], x[1]['intronEnd']['pos'])))
+
+    return intron_dict
 
 
 def getAlignmentLists(alignmentFile):
     print('--- Reading alignmentFile')
-    nonSplit_alignment_list = []
-    split_alignment_list = []
+    alignment_list = []
 
     for readID, alnObjList in getMultiMAFEntries_all(alignmentFile):
         # prerequisite:
         # alnObjList is already sorted
         # according to + strand's coordinates
 
-        # if the read is non-split 
-        if len(alnObjList)==1:
+        # if the read is non-split
+        if len(alnObjList) == 1:
             aln = alnObjList[0]
-            alnStart = (aln.gChr, aln.gStart)
-            alnEnd = (aln.gChr, aln.gEnd)
-            leftCoor = min([alnStart, alnEnd],
-                          key=lambda a: (a[0], a[1]))
-            rightCoor = max([alnStart, alnEnd],
-                          key=lambda a: (a[0], a[1]))
-            nonSplit_alignment_list.append((readID,
-                                            aln,
-                                            (leftCoor, rightCoor)))
+            alignment_list.append((readID,
+                                   [aln],
+                                   (aln, aln),
+                                   (None, None)))
         # if the read is split
         else:
             '''
@@ -134,42 +142,123 @@ def getAlignmentLists(alignmentFile):
                     aln1_end = (aln1.gChr, aln1.gEnd)
                     aln2_start = (aln2.gChr, aln2.gStart)
                     aln2_end = (aln2.gChr, aln2.gEnd)
-                    leftPos = min([aln1_start, aln1_end, aln2_start, aln2_end],
-                                  key=lambda a: (a[0], a[1]))
-                    if (leftPos == aln1_start or leftPos == aln1_end):
-                        alnLeft = aln1
-                        alnRight = aln2
+                    leftCoor = min([aln1_start, aln1_end,
+                                    aln2_start, aln2_end],
+                                   key=lambda a: (a[0], a[1]))
+                    rightCoor = max([aln1_start, aln1_end,
+                                     aln2_start, aln2_end],
+                                    key=lambda a: (a[0], a[1]))
+                    if (leftCoor == aln1_start or leftCoor == aln1_end):
+                        leftAln = aln1
                     else:
-                        alnLeft = aln2
-                        alnRight = aln1
-                    intronStart, intronEnd = getIntronCoord(readStrand, aln1, aln2)
-                    split_alignment_list.append((readID,
-                                                (aln1, aln2),
-                                                (alnLeft, alnRight),
-                                                (intronStart, intronEnd)))
-        print('--- Sorting nonSplit_alignment_list')
-        # sort nonSplit_alignment_list
-        nonSplit_alignment_list.sort(key=lambda x: (x[2][0], x[2][1]))
-        print('--- Sorting split_alignment_list')
-        # sort split_alignment_list
-        split_alignment_list.sort(key=lambda x: ((x[2][0].gChr, min(x[2][0].gStart, x[2][0].gEnd)),
-                                                 (x[2][1].gChr, min(x[2][1].gStart, x[2][1].gEnd))))
+                        leftAln = aln2
+                    if (rightCoor == aln1_start or rightCoor == aln1_end):
+                        rightAln = aln1
+                    else:
+                        rightAln = aln2
 
-        return nonSplit_alignment_list, split_alignment_list
+                    intronStart, intronEnd = getIntronCoord(readStrand,
+                                                            aln1, aln2)
+                    alignment_list.append((readID,
+                                           [aln1, aln2],
+                                           (leftAln, rightAln),
+                                           (intronStart, intronEnd)))
+        print('--- Sorting alignment_list')
+        # sort alignment_list by the rightCoor
+        alignment_list.sort(key=lambda x: (x[2][1][0], x[2][1][1]))
+
+        return alignment_list
 
 
-def getAlternativeSpilcingGroups(intron_list, nonSplit_alignment_list, split_alignment_list, outputFile):
+def rightStart(alignmentTuple):
     '''
-    nonSplit_alignment_list: a list of (readID, alnObj, (leftCoor, rightCoor))
-    split_alignment_list: a list of (readID, (aln1, aln2), (alnLeft, alnRight), (intronStart, intronEnd))
+    returns the start pos of the right-side alignment
     '''
-    print('--- Processing non-split alignments')
+    return (alignmentTuple[2][1].gChr, alignmentTuple[2][1].gStart)
 
-    print('--- Processing split alignments')
 
-    j = 0
-    for i in range(len(intron_list)):
-        while()
+def rightEnd(alignmentTuple):
+    '''
+    returns the end pos of the right-side alignment
+    '''
+    return (alignmentTuple[2][1].gChr, alignmentTuple[2][1].gEnd)
+
+
+def leftStart(alignmentTuple):
+    '''
+    returns the start pos of the left-side alignment
+    '''
+    return (alignmentTuple[2][0].gChr, alignmentTuple[2][0].gStart)
+
+
+def start(intronInfo):
+    '''
+    returns the start pos of the intron
+    '''
+    return (intronInfo['intronStart']['chr'],
+            intronInfo['intronStart']['pos'])
+
+
+def end(intronInfo):
+    '''
+    returns the end pos of the intron
+    '''
+    return (intronInfo['intronEnd']['chr'],
+            intronInfo['intronEnd']['pos'])
+
+
+def alignedPartsOverlap(alignmentTuple, intronInfo):
+    '''
+    returns True if there is an alignment overlapping with the intron
+    returns False if there is no alignment overlapping with the intron
+    '''
+    def diff(coord1, coord2):
+        assert coord1[0] == coord2[0]
+        return coord1[1]-coord2[1]
+
+    overlap = False
+    intronStart = (intronInfo['intronStart']['chr'],
+                   intronInfo['intronStart']['pos'])
+    intronEnd = (intronInfo['intronEnd']['chr'],
+                 intronInfo['intronEnd']['pos'])
+    intronLeft = min(intronStart, intronEnd)
+    intronRight = max(intronStart, intronEnd)
+    for aln in alignmentTuple[1]:
+        alnStart = (aln.gChr, aln.gStart)
+        alnEnd = (aln.gChr, aln.gEnd)
+        alnLeft = min(alnStart, alnEnd)
+        alnRight = max(alnStart, alnEnd)
+        s1 = diff(intronLeft, alnLeft)*diff(intronRight, alnRight)
+        s2 = diff(intronLeft, alnRight)*diff(intronRight, alnLeft)
+        if intronLeft <= alnRight
+
+def getAlternativeSpilcingGroups(intron_dict, alignment_list, outputFile):
+    '''
+    alignment_list: a list of (readID, [aln1, aln2], (leftAlnObj, rightAlnObj),
+                               (intronStart, intronEnd))
+    '''
+    print('--- Grouping alignments')
+
+    bookMark = 0
+    i = 0
+    for intronName, targetIntron in intron_dict.items():
+        while (bookMark < len(alignment_list) and
+               rightEnd(alignment_list[bookMark]) < start(targetIntron)):
+            bookMark += 1
+            i = bookMark
+            while (bookMark < len(alignment_list) and
+                   rightStart(alignment_list[bookMark]) < end(targetIntron)):
+                if (alignedPartsOverlap(alignment_list[i], targetIntron)):
+                    # add to dict
+                    pass
+                bookMark += 1
+                i = bookMark
+            while (i < len(alignment_list) and
+                   leftStart(alignment_list[i]) < end(targetIntron)):
+                if (alignedPartsOverlap(alignment_list[i], targetIntron)):
+                    # add to dict
+                    pass
+                i += 1
 
 
 if __name__ == '__main__':
@@ -188,14 +277,14 @@ if __name__ == '__main__':
     '''
     Load intronJsonFile and sort the intron data
     '''
-    intron_list = getIntronList(args.intronJsonFile)
+    intron_dict = getIntronDict(args.intronJsonFile)
 
     '''
     Load alignmentFile and sort the alignment data
     '''
-    nonSplit_alignment_list, split_alignment_list = getAlignmentLists(args.alignmentFile)
+    alignment_list = getAlignmentLists(args.alignmentFile)
 
     '''
     Get alternative splicing info
     '''
-    getAlternativeSpilcingGroups(intron_list, nonSplit_alignment_list, split_alignment_list, args.outputFile)
+    getAlternativeSpilcingGroups(intron_dict, alignment_list, args.outputFile)
