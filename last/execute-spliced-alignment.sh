1a8892154c82929840ed3a0401dea3630852915e
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-05-25 02:29:38 +0000

    bug fixed. still there might be other bugs...

--- last/execute-spliced-alignment.sh
+++ last/execute-spliced-alignment.sh
@@ -8,10 +8,10 @@ fi
 
 DATE=$1
 
-dbNAME="OKI2018_I69_1.0db_${DATE}"
-train_emb="train_OKI2018_I69_1.0_ERR4570985_$DATE.out"
-train_imm="train_OKI2018_I69_1.0_ERR4570986_$DATE.out"
-train_mat="train_OKI2018_I69_1.0_ERR4570987_$DATE.out"
+dbNAME="OKI2018_I69_1.0db"
+train_emb="train_OKI2018_I69_1.0_ERR4570985.out"
+train_imm="train_OKI2018_I69_1.0_ERR4570986.out"
+train_mat="train_OKI2018_I69_1.0_ERR4570987.out"
 al_emb1="lastsplitOKI2018_I69_1.0_ERR4570985_1_filtered_trimmed_sorted_postmask_$DATE.maf"
 al_emb2="lastsplitOKI2018_I69_1.0_ERR4570985_2_filtered_trimmed_sorted_postmask_$DATE.maf"
 al_emb_pairprob="lastsplitOKI2018_I69_1.0_ERR4570985_filtered_trimmed_sorted_postmask_pairprob_$DATE.maf"
@@ -73,28 +73,28 @@ echo "---lastal"
 # - spliced alignment of Read1
 if [ ! -e embryos/$al_emb1 ]; then
         echo "doing spliced-alignment on embryos (Read1)"
-        lastal -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p embryos/$train_emb $dbNAME/$dbNAME ../rna-seq-data/embryos/ERR4570985_1_filtered_trimmed_sorted.fastq | last-postmask > embryos/$al_emb1
+        lastal -i1 -P8 -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p embryos/$train_emb $dbNAME/$dbNAME ../rna-seq-data/embryos/ERR4570985_1_filtered_trimmed_sorted.fastq | last-postmask > embryos/$al_emb1
 else
         echo "embryos/$al_emb1 already exists"
 fi
 # - spliced alignment of Read2
 if [ ! -e embryos/$al_emb2 ]; then
         echo "doing spliced-alignment on embryos (Read2)"
-        lastal -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p embryos/$train_emb $dbNAME/$dbNAME ../rna-seq-data/embryos/ERR4570985_2_filtered_trimmed_sorted.fastq | last-postmask > embryos/$al_emb2
+        lastal -i1 -P8 -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p embryos/$train_emb $dbNAME/$dbNAME ../rna-seq-data/embryos/ERR4570985_2_filtered_trimmed_sorted.fastq | last-postmask > embryos/$al_emb2
 else
         echo "embryos/$al_emb2 already exists"
 fi
 # - last-pair-probs Read1, Read2
 if [ ! -e embryos/$al_emb_pairprob ]; then
         echo "running last-pair-probs"
-        last-pair-probs -r $al_emb1 $al_emb2 > $al_emb_pairprob 
+        last-pair-probs -r embryos/$al_emb1 embryos/$al_emb2 > embryos/$al_emb_pairprob 
 else
         echo "embryos/$al_emb_pairprob already exists"
 fi
 # - maf-convert sam
 if [ ! -e embryos/$sam_emb ]; then
         echo "converting maf to sam"
-        maf-convert -j1e5 -d sam $al_emb_pairprob > $sam_emb 
+        maf-convert -j1e5 -d sam embryos/$al_emb_pairprob > embryos/$sam_emb 
 else
         echo "embryos/$sam_emb already exists"
 fi
@@ -103,28 +103,28 @@ fi
 # - spliced alignment of Read1
 if [ ! -e immatureAdults/$al_imm1 ]; then
         echo "doing spliced-alignment on immature adults (Read1)"
-        lastal -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p immatureAdults/$train_imm $dbNAME/$dbNAME ../rna-seq-data/immatureAdults/ERR4570986_1_filtered_trimmed_sorted.fastq | last-postmask > immatureAdults/$al_imm1
+        lastal -i1 -P8 -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p immatureAdults/$train_imm $dbNAME/$dbNAME ../rna-seq-data/immatureAdults/ERR4570986_1_filtered_trimmed_sorted.fastq | last-postmask > immatureAdults/$al_imm1
 else
         echo "immatureAdults/$al_imm1 already exists"
 fi
 # - spliced alignment of Read2
 if [ ! -e immatureAdults/$al_imm2 ]; then
         echo "doing spliced-alignment on immature adults (Read2)"
-        lastal -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p immatureAdults/$train_imm $dbNAME/$dbNAME ../rna-seq-data/immatureAdults/ERR4570986_2_filtered_trimmed_sorted.fastq | last-postmask > immatureAdults/$al_imm2
+        lastal -i1 -P8 -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p immatureAdults/$train_imm $dbNAME/$dbNAME ../rna-seq-data/immatureAdults/ERR4570986_2_filtered_trimmed_sorted.fastq | last-postmask > immatureAdults/$al_imm2
 else
         echo "immatureAdults/$al_imm2 already exists"
 fi
 # - last-pair-probs Read1, Read2
 if [ ! -e immatureAdults/$al_imm_pairprob ]; then
         echo "running last-pair-probs"
-        last-pair-probs -r $al_imm1 $al_imm2 > $al_imm_pairprob 
+        last-pair-probs -r immatureAdults/$al_imm1 immatureAdults/$al_imm2 > immatureAdults/$al_imm_pairprob 
 else
         echo "embryos/$al_imm_pairprob already exists"
 fi
 # - maf-convert sam
 if [ ! -e immatureAdults/$sam_imm ]; then
         echo "convert maf to sam"
-        maf-convert -j1e5 -d sam $al_imm_pairprob > $sam_imm 
+        maf-convert -j1e5 -d sam immatureAdults/$al_imm_pairprob > immatureAdults/$sam_imm 
 else
         echo "immatureAdults/$sam_imm already exists"
 fi
@@ -133,28 +133,28 @@ fi
 # - spliced alignment of Read1
 if [ ! -e maturedAdults/$al_mat1 ]; then
         echo "doing spliced-alignment on matured adults (Read1)"
-        lastal -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p maturedAdults/$train_mat $dbNAME/$dbNAME ../rna-seq-data/maturedAdults/ERR4570987_1_filtered_trimmed_sorted.fastq | last-postmask > maturedAdults/$al_mat1
+        lastal -i1 -P8 -D10 --split-d 0 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p maturedAdults/$train_mat $dbNAME/$dbNAME ../rna-seq-data/maturedAdults/ERR4570987_1_filtered_trimmed_sorted.fastq | last-postmask > maturedAdults/$al_mat1
 else
         echo "maturedAdults/$al_mat1 already exists"
 fi
 # - spliced alignment of Read2
 if [ ! -e maturedAdults/$al_mat2 ]; then
         echo "doing spliced-alignment on matured adults (Read2)"
-        lastal -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p maturedAdults/$train_mat $dbNAME/$dbNAME ../rna-seq-data/maturedAdults/ERR4570987_2_filtered_trimmed_sorted.fastq | last-postmask > maturedAdults/$al_mat2
+        lastal -i1 -P8 -D10 --split-d 1 --split-M 3.78 --split-S 0.065 --split-m 0.5 -p maturedAdults/$train_mat $dbNAME/$dbNAME ../rna-seq-data/maturedAdults/ERR4570987_2_filtered_trimmed_sorted.fastq | last-postmask > maturedAdults/$al_mat2
 else
         echo "maturedAdults/$al_mat2 already exists"
 fi
 # - last-pair-probs Read1, Read2
-if [ ! -e maturedAdults/$al_emb_pairprob ]; then
+if [ ! -e maturedAdults/$al_mat_pairprob ]; then
         echo "running last-pair-probs"
-        last-pair-probs -r $al_emb1 $al_emb2 > $al_emb_pairprob 
+        last-pair-probs -r maturedAdults/$al_mat1 maturedAdults/$al_mat2 > maturedAdults/$al_mat_pairprob 
 else
-        echo "embryos/$al_emb_pairprob already exists"
+        echo "maturedAdults/$al_mat_pairprob already exists"
 fi
 # - maf-convert sam
 if [ ! -e maturedAdults/$sam_mat ]; then
         echo "converting maf to sam"
-        maf-convert -j1e5 -d sam $al_mat_pairprob > $sam_mat 
+        maf-convert -j1e5 -d sam maturedAdults/$al_mat_pairprob > maturedAdults/$sam_mat 
 else
         echo "maturedAdults/$sam_mat already exists"
 fi
