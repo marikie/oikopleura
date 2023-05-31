da9f5bab8ca9d6b95f15684c5ee4124e23d6fbe7
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-05-31 01:39:57 +0000

    workable. I still want to set the output file though.

--- trinity/execute_trinity_denovo.sh
+++ trinity/execute_trinity_denovo.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]; then
-        echo "You need 6 argument" 1>&2
+if [ $# -ne 1 ]; then
+        echo "You need 1 argument" 1>&2
         echo "- today's date" 1>&2
-        echo "- seqType (fa or fq)" 1>&2
-        echo "- library type (paired: RF or FR, single: F or R)" 1>&2
-        echo "- path of Read1 file" 1>&2
-        echo "- path of Read2 file" 1>&2
-        echo "- developping stage of oiks (embryo, immature, or matured)" 1>&2
         exit 1
 fi
 # --CPU, --max_memory, --min_contig_length
 
 DATE=$1
-seqType=$2
-libType=$3
-read1Path=$4
-read2Path=$5
-devStage=$6
-outputDir="trinity_denovo_${devStage}_$DATE"
+seqType='fq'
+libType='FR'
+readDir_emb="/big/mrk/oikopleura/rna-seq-data/embryos"
+read1Path_emb="$readDir_emb/ERR4570985_1_filtered_trimmed_sorted.fastq"
+read2Path_emb="$readDir_emb/ERR4570985_2_filtered_trimmed_sorted.fastq"
+devStage_emb='embryos'
+#outputDir="/big/mrk/oikopleura/trinity/trinity_denovo_${devStage_emb}_$DATE"
 
 #if [ ! -d  $outputDir ]; then
 #        mkdir $outputDir
 #fi
 
+export SINGULARITY_BIND="$readDir:/big/mrk/oikopleura/rna-seq-data/embryos"
+#export SINGULARITY_BIND="$outputDir:/big/mrk/oikopleura/trinity/trinity_denovo_${devStage_emb}_$DATE"
+
+# embryos
 singularity exec -e $HOME/.local/src/trinityrnaseq.v2.15.1.simg  Trinity \
 --seqType $seqType  \
---left $read1Path --right $read2Path --SS_lib_type $libType \
+--left $read1Path_emb --right $read2Path_emb --SS_lib_type $libType \
 --CPU 10 --min_contig_length 100 --max_memory 50G \
---verbose --output $outputDir
+--verbose
+#--output $outputDir
