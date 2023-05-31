61c11a33f6748c513e6464123049d428c6909195
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-05-31 06:37:54 +0000

    updated to set output dir

--- trinity/execute_trinity_denovo.sh
+++ trinity/execute_trinity_denovo.sh
@@ -14,19 +14,21 @@ readDir_emb="/big/mrk/oikopleura/rna-seq-data/embryos"
 read1Path_emb="$readDir_emb/ERR4570985_1_filtered_trimmed_sorted.fastq"
 read2Path_emb="$readDir_emb/ERR4570985_2_filtered_trimmed_sorted.fastq"
 devStage_emb='embryos'
-#outputDir="/big/mrk/oikopleura/trinity/trinity_denovo_${devStage_emb}_$DATE"
+outputDir="/big/mrk/oikopleura/trinity/trinity_denovo_${devStage_emb}_$DATE"
 
-#if [ ! -d  $outputDir ]; then
-#        mkdir $outputDir
-#fi
+if [ ! -d  $outputDir ]; then
+        echo "making output dir"
+        mkdir $outputDir
+else
+        echo "$outputDir already exists."
+fi
 
-export SINGULARITY_BIND="$readDir:/big/mrk/oikopleura/rna-seq-data/embryos"
-#export SINGULARITY_BIND="$outputDir:/big/mrk/oikopleura/trinity/trinity_denovo_${devStage_emb}_$DATE"
+export SINGULARITY_BIND="$readDir:$readDir,$outputDir:$outputDir"
 
 # embryos
 singularity exec -e $HOME/.local/src/trinityrnaseq.v2.15.1.simg  Trinity \
 --seqType $seqType  \
 --left $read1Path_emb --right $read2Path_emb --SS_lib_type $libType \
 --CPU 10 --min_contig_length 100 --max_memory 50G \
---verbose
-#--output $outputDir
+--verbose \
+--output $outputDir
--- trinity/execute_trinity_gguided.sh
+++ trinity/execute_trinity_gguided.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then
-        echo "You need 5 argument" 1>&2
+if [ $# -ne 1 ]; then
+        echo "You need 1 argument" 1>&2
         echo "- today's date" 1>&2
-        echo "- library type (paired: RF or FR, single: F or R)" 1>&2
-        echo "- path of alignment.bam file" 1>&2
         exit 1
 fi
 # --CPU, --max_memory, --min_contig_length, genome_guided_max_intron
 
 DATE=$1
-libType=$2
-alignmentBamFile=$3
-outputDir="$HOME/big_oiks/trinity_gguided_${alignmentBamFile:0:-4}_$DATE"
+libType='FR'
+alignmentBamFileDir="/big/mrk/oikopleura/last/embryos"
+alignmentBamFile="$alignmentBamFileDir/lastsplitOKI2018_I69_1.0_ERR4570985_filtered_trimmed_sorted_postmask_pairprob_20230525.sort.bam"
+devStage_emb='embryos'
+outputDir="/big/mrk/oikopleura/trinity/trinity_gguided_${devStage_emb}_$DATE"
 
 if [ ! -d  $outputDir ]; then
+        echo "making output dir"
         mkdir $outputDir
+else
+        echo "$outputDir already exists."
 fi
+
+export SINGULARITY_BIND="$alignmentBamFileDir:$alignmentBamFileDir,$outputDir:$outputDir"
+echo "$SINGULARITY_BIND"
 
 singularity exec -e $HOME/.local/src/trinityrnaseq.v2.15.1.simg  Trinity \
         --genome_guided_bam $alignmentBamFile \
         --genome_guided_max_intron 100000 \
         --SS_lib_type $libType \
-        --max_memory 50G --CPU 10
+        --max_memory 50G --CPU 10 \
+        --verbose \
+        --output $outputDir
