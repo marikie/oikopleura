79cd2df94894390ae15cff5ccb22cb82cdb1332b
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-06-20 06:07:29 +0000

    added sortAlignmentsByQueryCoord.py

---
+++ analysis/sortAlignmentsByQueryCoord.py
@@ -0,0 +1,35 @@
+'''
+Sort .maf file entries by the coordinates of queries
+
+Input: .maf file
+Output: .maf file
+'''
+import argparse
+from Util import getMAFBlock
+from Alignment import Alignment
+
+
+def main(alignmentFile):
+    alnFileHandle = open(alignmentFile)
+    # get all the MAF entries
+    mafEntries_all = []
+    for mafEntry in getMAFBlock(alnFileHandle):
+        mafEntries_all.append(Alignment.fromMAFEntry(mafEntry))
+
+    # sort mafEntries_all by query's coordinate
+
+
+
+
+if __name__ == '__main__':
+    '''
+    File Parsing
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('alignmentFile',
+                        help='a .maf alignment file')
+    args = parser.parse_args()
+    '''
+    MAIN
+    '''
+    main(args.alignmentFile)
