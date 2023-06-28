1284e077837523ab0bf9d7d993353923fae4bcc8
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-06-28 16:08:12 +0900

    completed dotplot part, bug fixed: convert to + strand coord

--- analysis/searchSquashedExons.py
+++ analysis/searchSquashedExons.py
@@ -7,22 +7,22 @@ import argparse
 from Util import getMAFBlock
 from Alignment import Alignment
 import subprocess
-import os
+from Util import convert2CoorOnOppositeStrand
 
 
 def segmentOverlap(a1, b1, a2, b2):
     s1 = (a1 - a2) * (b1 - b2)
     s2 = (a1 - b2) * (b1 - a2)
     # if not no-overlap
-    if not (s1 > 0 and s2 > 0):
+    if ((not (s1 > 0 and s2 > 0)) and (not (s1 > 0 and s2 == 0))):
         return True
     else:
         return False
 
 
-def overlap(aln, start, end):
-    if (aln.rID == start[0] and segmentOverlap(aln.rStart, aln.rEnd,
-                                               start[1], end[1])):
+def overlap(alnrID, alnStart, alnEnd, start, end):
+    if (alnrID == start[0] and segmentOverlap(alnStart, alnEnd,
+                                              start[1], end[1])):
         return True
     else:
         return False
@@ -36,18 +36,24 @@ def getOvlGroups(alignmentFile):
     ovlList = []
     for mafEntry in getMAFBlock(alnFileHandle):
         aln = Alignment.fromMAFEntry(mafEntry)
+        # set coord to + strand coord
+        if aln.rStrand == '-':
+            # convert coord to + strand coord
+            alnStart, alnEnd = convert2CoorOnOppositeStrand(aln)
+        else:
+            alnStart, alnEnd = (aln.rStart, aln.rEnd)
 
         if not start:
-            start = (aln.rID, aln.rStart)
+            start = (aln.rID, alnStart)
         if not end:
-            end = (aln.rID, aln.rEnd)
+            end = (aln.rID, alnEnd)
         
-        if overlap(aln, start, end):
+        if overlap(aln.rID, alnStart, alnEnd, start, end):
             # add to ovlList
             ovlList.append(aln)
-            if (end[0] == aln.rID and end[1] < aln.rEnd):
+            if (end[0] == aln.rID and end[1] < alnEnd):
                 # update "end"
-                end = (aln.rID, aln.rEnd)
+                end = (aln.rID, alnEnd)
             else:
                 pass
         else:
@@ -55,10 +61,11 @@ def getOvlGroups(alignmentFile):
                 # add ovlList to allGroupsList
                 yield ovlList
             else:
+                # print('ovlList <= 1')
                 pass
             # new start, end, ovlList
-            start = (aln.rID, aln.rStart)
-            end = (aln.rID, aln.rEnd)
+            start = (aln.rID, alnStart)
+            end = (aln.rID, alnEnd)
             ovlList = [aln]
     else:
         if len(ovlList) > 1:
@@ -68,22 +75,42 @@ def getOvlGroups(alignmentFile):
 
 
 def makeDotplotFiles(alignmentFile, outputDirPath):
-    # make a dir
-    subprocess.run(['mkdir', outputDirPath])
-    # change dir to outputDirPath
-    subprocess.run(['cd', outputDirPath])
+    p1 = subprocess.run(['ls', outputDirPath], check=False)
+    if p1.returncode != 0:
+        # make a dir
+        subprocess.run(['mkdir', outputDirPath])
 
     for ovlGroup in getOvlGroups(alignmentFile):
-        outputFileName = ovlGroup[0].rID + '_' + str(ovlGroup[0].rStart) + '-'\
-                            + str(ovlGroup[-1].rEnd) + '.png'
-        with open(outputDirPath + '/temp.maf', 'a') as f:
+        if ovlGroup[0].rStrand == '-':
+            # convet to + strand coord
+            firstStart = convert2CoorOnOppositeStrand(ovlGroup[0])[0]
+        else:
+            firstStart = ovlGroup[0].rStart
+
+        if ovlGroup[-1].rStrand == '-':
+            # convert to + strand coord
+            lastEnd = convert2CoorOnOppositeStrand(ovlGroup[-1])[1]
+        else:
+            lastEnd = ovlGroup[-1].rEnd
+
+        outputFileName_png = ovlGroup[0].rID + '_' + str(firstStart) \
+                            + '-' \
+                            + str(lastEnd) + '.png'
+        outputFileName_maf = ovlGroup[0].rID + '_' + str(firstStart) \
+                            + '-' \
+                            + str(lastEnd) + '.maf'
+        with open(outputDirPath + '/' + outputFileName_maf, 'a') as f:
             for aln in ovlGroup:
                 f.write(aln._MAF())
             else:
+                # f.write('---- group end ----\n')
                 f.flush()
                 # cmd: last-dotplot temp.maf outputFileName
-                subprocess.run(['last-dotplot', 'temp.maf', outputFileName])
-                subprocess.run(['rm', outputDirPath + '/temp.maf'])
+                subprocess.run(['last-dotplot',
+                                '--labels1=3',
+                                '--labels2=3',
+                                outputDirPath + '/' + outputFileName_maf,
+                                outputDirPath + '/' + outputFileName_png])
 
 
 if __name__ == '__main__':
