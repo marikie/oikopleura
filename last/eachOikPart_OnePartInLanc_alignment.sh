31854eeef2baaad2e9d79ed09300be536f9e9e52
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-07-06 12:54:08 +0900

    finished writing

--- last/eachOikPart_OnePartInLanc_alignment.sh
+++ last/eachOikPart_OnePartInLanc_alignment.sh
@@ -13,15 +13,13 @@ maf="eachOikPart_OnePartInLanc_alignment_$DATE.maf"
 sam="eachOikPart_OnePartInLanc_alignment_$DATE.sam"
 pngFile="eachOikPart_OnePartInLanc_alignment_$DATE.png"
 
-cd ~/big_oiks/lanc_oik_last
-
 # lastdb
 echo "---lastdb"
-if [ ! -d /home/mrk/big_oiks/lanc_oik_last/$dbName ]; then
+if [ ! -d /home/mrk/oikdata/lanc_oik_last/$dbName ]; then
         echo "making lastdb"
         mkdir $dbName
         cd $dbName
-        lastdb -P8 -uMAM8 $dbName ~/big_oiks/lancelets/genome_assemblies_branchiostoma_floridae/ncbi-genomes-2023-06-13/GCF_000003815.2_Bfl_VNyyK_genomic.fna 
+        lastdb -P8 -uMAM8 $dbName ~/oikdata/lancelets/genome_assemblies_branchiostoma_floridae/ncbi-genomes-2023-06-13/GCF_000003815.2_Bfl_VNyyK_genomic.fna 
         cd ..
 else
         echo "$dbName already exists"
@@ -31,42 +29,33 @@ fi
 echo "--last-train"
 if [ ! -e $trainFile ]; then
         echo "doing last-train"
-        last-train -P8 --revsym -E0.05 $dbName/$dbName ~/big_oiks/last/OKI2018_I69_1.0.removed_chrUn.fa > $trainFile
+        last-train -P8 --revsym -E0.05 $dbName/$dbName ~/oikdata/last/OKI2018_I69_1.0.removed_chrUn.fa > $trainFile
 else
         echo "$trainFile already exists"
 fi
 
-# lastal 1
-echo "---lastal 1"
-if [ ! -e $maf1 ]; then 
+# lastal 
+echo "---lastal"
+if [ ! -e $maf ]; then 
         echo "doing lastal phase 1"
-        lastal -E0.05 --split-f=MAF+ -p $trainFile $dbName/$dbName ~/big_oiks/last/OKI2018_I69_1.0.removed_chrUn.fa > $maf1
+        lastal -E0.05 --split-f=MAF+ -p $trainFile $dbName/$dbName ~/oikdata/last/OKI2018_I69_1.0.removed_chrUn.fa | last-postmask > $maf
 else
-        echo "$maf1 already exists"
-fi
-
-# lastal 2
-echo "---lastal 2"
-if [ ! -e $maf2 ]; then
-        echo "doing lastal phase 2"
-        last-split -r -m1e-5 $maf1 | last-postmask > $maf2
-else
-        echo "$maf2 already exists"
+        echo "$maf already exists"
 fi
 
 # maf-convert sam
-if [ ! -e $sam2 ]; then
+if [ ! -e $sam ]; then
         echo "converting maf to sam"
-        maf-convert -j1e5 -d sam $maf2 > $sam2
+        maf-convert -j1e5 -d sam $maf > $sam
 else
-        echo "$sam2 already exists"
+        echo "$sam already exists"
 fi
 
 # last-dotplot
 echo "---last-dotplot"
 if [ ! -e $pngFile ]; then
         echo "making $pngFile"
-        last-dotplot $maf2 $pngFile
+        last-dotplot -a ~/oikdata/lancelets/ncbi_dataset/data/GCF_000003815.2/genomic.gff $maf $pngFile
 else
         echo "$pngFile already exists"
 fi
