78934fb8847c753bc75ed1ad59b4961889622b6a
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-07-25 13:55:20 +0900

    started to modify: to output maffiles with 4 categories

--- analysis/searchAlignedSegmentsClose2EachOther.py
+++ analysis/searchAlignedSegmentsClose2EachOther.py
@@ -1,7 +1,10 @@
 '''
 * Assuming no overlapped segments on the query
+Search aligned segments that are close to each other (<= allowedLen)
+on the query chromosom.
 Input: .maf file sorted by query's + strand coordinate
-Output: dotplot.png files
+Output: sameOnRef_sameOnQuery/mafFiles, diffOnRef_sameOnQuery/mafFiles,
+        sameOnRef_diffOnQuery/mafFiles, diffOnRef_diffOnQuery/mafFiles
 '''
 
 import argparse
@@ -77,8 +80,8 @@ def getCloseSegs(alignmentFile, allowedLen):
             pass
 
 
-def makeDotplotFiles(alignmentFile, annoFile_1, annoFile_2,
-                     allowedLen, outputDirPath):
+def outputMAFFiles(alignmentFile, annoFile_Ref, annoFile_Query,
+                    allowedLen, outputDirPath):
     # print('before subprocess')
     p1 = subprocess.run(['ls', outputDirPath], capture_output=True)
     # print('after subprocess')
@@ -86,20 +89,22 @@ def makeDotplotFiles(alignmentFile, annoFile_1, annoFile_2,
     if p1.returncode != 0:
         # make a dir
         subprocess.run(['mkdir', outputDirPath])
+        subprocess.run(['mkdir', outputDirPath + '/sameOnRef_sameOnQuery'])
+        subprocess.run(['mkdir', outputDirPath + '/sameOnRef_diffOnQuery'])
+        subprocess.run(['mkdir', outputDirPath + '/diffOnRef_sameOnQuery'])
+        subprocess.run(['mkdir', outputDirPath + '/diffOnRef_diffOnQuery'])
 
     for closeSegGroup in getCloseSegs(alignmentFile, allowedLen):
         # convet to + strand coord
-        firstStart = setToPlusCoord(closeSegGroup[0])[0]
-        print('firstStart: ', firstStart)
-        lastEnd = setToPlusCoord(closeSegGroup[-1])[1]
-        print('lastEnd: ', lastEnd)
+        firstElemStart = setToPlusCoord(closeSegGroup[0])[0]
+        print('firstStart: ', firstElemStart)
+        lastElemEnd = setToPlusCoord(closeSegGroup[-1])[1]
+        print('lastEnd: ', lastElemEnd)
 
-        outputFileName_png = closeSegGroup[0].rID + '_' + str(firstStart) \
+        outputFileName_maf = closeSegGroup[0].rID + '_' + str(firstElemStart) \
                             + '-' \
-                            + str(lastEnd) + '.png'
-        outputFileName_maf = closeSegGroup[0].rID + '_' + str(firstStart) \
-                            + '-' \
-                            + str(lastEnd) + '.maf'
+                            + str(lastElemEnd) + '.maf'
+
         with open(outputDirPath + '/' + outputFileName_maf, 'a') as f:
             for aln in closeSegGroup:
                 f.write(aln._MAF())
@@ -111,12 +116,52 @@ def makeDotplotFiles(alignmentFile, annoFile_1, annoFile_2,
                                 '--labels1=3',
                                 '--labels2=3',
                                 '-a',
-                                annoFile_1,
+                                annoFile_Ref,
                                 '-b',
-                                annoFile_2,
-                                outputDirPath + '/' + outputFileName_maf,
-                                outputDirPath + '/' + outputFileName_png])
+                                annoFile_Query,
+                                outputDirPath + '/' + outputFileName_maf])
 
+
+# def makeDotplotFiles(alignmentFile, annoFile_1, annoFile_2,
+#                      allowedLen, outputDirPath):
+#     # print('before subprocess')
+#     p1 = subprocess.run(['ls', outputDirPath], capture_output=True)
+#     # print('after subprocess')
+#     # print(p1.returncode)
+#     if p1.returncode != 0:
+#         # make a dir
+#         subprocess.run(['mkdir', outputDirPath])
+#
+#     for closeSegGroup in getCloseSegs(alignmentFile, allowedLen):
+#         # convet to + strand coord
+#         firstStart = setToPlusCoord(closeSegGroup[0])[0]
+#         print('firstStart: ', firstStart)
+#         lastEnd = setToPlusCoord(closeSegGroup[-1])[1]
+#         print('lastEnd: ', lastEnd)
+#
+#         outputFileName_png = closeSegGroup[0].rID + '_' + str(firstStart) \
+#                             + '-' \
+#                             + str(lastEnd) + '.png'
+#         outputFileName_maf = closeSegGroup[0].rID + '_' + str(firstStart) \
+#                             + '-' \
+#                             + str(lastEnd) + '.maf'
+#         with open(outputDirPath + '/' + outputFileName_maf, 'a') as f:
+#             for aln in closeSegGroup:
+#                 f.write(aln._MAF())
+#             else:
+#                 # f.write('---- group end ----\n')
+#                 f.flush()
+#                 # cmd: last-dotplot temp.maf outputFileName
+#                 subprocess.run(['last-dotplot',
+#                                 '--labels1=3',
+#                                 '--labels2=3',
+#                                 '-a',
+#                                 annoFile_1,
+#                                 '-b',
+#                                 annoFile_2,
+#                                 outputDirPath + '/' + outputFileName_maf,
+#                                 outputDirPath + '/' + outputFileName_png])
+#
 
 if __name__ == '__main__':
     '''
