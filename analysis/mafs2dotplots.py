cee729e05a463ba2c96e6ef74bfca299b81b15e3
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-10-03 19:28:19 +0900

    modified

--- analysis/mafs2dotplots.py
+++ analysis/mafs2dotplots.py
@@ -1,11 +1,12 @@
-'''
+"""
 Input:
-    A result dir path of searchAlignmentSegmentsClose2EachOther.py
+    path to the directory which has sub-directories that have /MAF directory under
 Output:
-    For each dir under the input dir,
+    For each sub dir under the input dir,
     read .maf files in /MAF dir,
-    and output .png files in /PNG dir.
-'''
+    and output _geneID.png and _protein.png files in /PNG dir.
+
+"""
 
 import argparse
 import subprocess
@@ -13,68 +14,96 @@ import os
 
 
 def makeDotPlotFiles(inputDirPath, annoFile_Reference, annoFile_Query):
-    p_ls_inDir = subprocess.run(['ls', inputDirPath], capture_output=True)
+    p_ls_inDir = subprocess.run(["ls", inputDirPath], capture_output=True)
 
-    subDirs = p_ls_inDir.stdout.decode('utf-8').strip().split('\n')
+    subDirs = p_ls_inDir.stdout.decode("utf-8").strip().split("\n")
 
     for subdir in subDirs:
-        outputDirPath = inputDirPath + '/' + subdir + '/PNG'
-        p_ls_outDir = subprocess.run(['ls', outputDirPath],
-                                     capture_output=True)
+        outputDirPath = inputDirPath + "/" + subdir + "/PNG"
+        p_ls_outDir = subprocess.run(["ls", outputDirPath], capture_output=True)
         if p_ls_outDir.returncode != 0:
             # make outDir
-            subprocess.run(['mkdir', outputDirPath])
+            subprocess.run(["mkdir", outputDirPath])
         else:
             pass
 
         # ls subdir/MAF
-        p_ls_maf = subprocess.run(['ls', inputDirPath + '/' + subdir + '/MAF'],
-                                  capture_output=True)
-        mafFiles = p_ls_maf.stdout.decode('utf-8').strip().split('\n')
-        if mafFiles[0] != '':
+        p_ls_maf = subprocess.run(
+            ["ls", inputDirPath + "/" + subdir + "/MAF"], capture_output=True
+        )
+        mafFiles = p_ls_maf.stdout.decode("utf-8").strip().split("\n")
+        # print(mafFiles)
+        if mafFiles[0] != "":
             for maffile in mafFiles:
-                pngfile = os.path.splitext(maffile)[0] + '.png'
-                maffilePath = inputDirPath + '/' + subdir + '/MAF/' + maffile
-                # make .png file
-                subprocess.run(['python',
-                                '../last/last-dotplot_mariko.py',
-                                '--sort1=3',
-                                '--strands1=1',
-                                '--border-color=silver',
-                                '--border-pixels=5',
-                                '--rot1=v',
-                                '--labels1=2',
-                                '--labels2=2',
-                                '--fontsize=10',
-                                '-a',
-                                annoFile_Reference,
-                                '-b',
-                                annoFile_Query,
-                                maffilePath,
-                                outputDirPath + '/' + pngfile
-                                ])
+                pngfile_geneID = os.path.splitext(maffile)[0] + "_geneID.png"
+                pngfile_protein = os.path.splitext(maffile)[0] + "_protein.png"
+                maffilePath = inputDirPath + "/" + subdir + "/MAF/" + maffile
+                # make _geneID.png file
+                subprocess.run(
+                    [
+                        "last-dotplot",
+                        "--sort1=3",
+                        "--strands1=1",
+                        "--border-color=silver",
+                        "--border-pixels=5",
+                        "--rot1=v",
+                        "--labels1=2",
+                        "--labels2=2",
+                        "--fontsize=10",
+                        "-a",
+                        annoFile_Reference,
+                        "-b",
+                        annoFile_Query,
+                        maffilePath,
+                        outputDirPath + "/" + pngfile_geneID,
+                    ]
+                )
+                # make _protein.png file
+                subprocess.run(
+                    [
+                        "python",
+                        "../last/last-dotplot_mariko.py",
+                        "--sort1=3",
+                        "--strands1=1",
+                        "--border-color=silver",
+                        "--border-pixels=5",
+                        "--rot1=v",
+                        "--labels1=2",
+                        "--labels2=2",
+                        "--fontsize=10",
+                        "-a",
+                        annoFile_Reference,
+                        "-b",
+                        annoFile_Query,
+                        maffilePath,
+                        outputDirPath + "/" + pngfile_protein,
+                    ]
+                )
         else:
             pass
 
 
-if __name__ == '__main__':
-    '''
+if __name__ == "__main__":
+    """
     File Parsing
-    '''
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputDirPath',
-                        help='path of the directory, which is \
-                        the result directory of \
-                        searchAlignedSegmentsClose2EachOther.py')
-    parser.add_argument('annoFile_Reference',
-                        help='path of an annotation file of \
-                        the reference')
-    parser.add_argument('annoFile_Query',
-                        help='path of an annotation file of \
-                        the query')
+    parser.add_argument(
+        "inputDirPath",
+        help="path of the directory, which has sub-directories having /MAF directory below",
+    )
+    parser.add_argument(
+        "annoFile_Reference",
+        help="path of an annotation file of \
+                        the reference",
+    )
+    parser.add_argument(
+        "annoFile_Query",
+        help="path of an annotation file of \
+                        the query",
+    )
     args = parser.parse_args()
-    '''
+    """
     MAIN
-    '''
-    makeDotPlotFiles(args.inputDirPath, args.annoFile_Reference,
-                     args.annoFile_Query)
+    """
+    makeDotPlotFiles(args.inputDirPath, args.annoFile_Reference, args.annoFile_Query)
