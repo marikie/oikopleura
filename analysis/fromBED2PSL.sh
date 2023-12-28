439fdb28e59c569eee5ce4081a2717ec032f020a
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-12-28 19:08:45 +0900

    modified

--- analysis/fromBED2PSL.sh
+++ analysis/fromBED2PSL.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-argNum=1
+argNum=2
 
 if [ $# -ne $argNum ]; then
         echo "You need $argNum arguments." 1>&2
         echo "- path to the target directory" 1>&2 # $1
+        echo "- path to the PSL alignment file" 1>&2 # $2
         exit 1
 fi
 
 targetDir=$1
+alnFile=$2
 
 cd $targetDir
 
-for file in $(ls ranno*\.out); do 
+for file in $(ls $targetDir); do 
   # echo "$file"
   if [ -f "$file" ]; then
     basename="$(basename "$file" ".out")"
     pslFile="$basename.psl"
     
     # echo "$pslFile"
-    awk 'NR == FNR {a[$4, $5, $6, $1, $2, $3]; next} ("chr_"$10, $12, $13, "chr_"$14, $16, $17) in a' $file ../../../eachOikPart_OnePartInLanc_alignment_20230706_sortedByQuery.psl > $pslFile
+    awk 'NR == FNR {a[$4, $5, $6, $1, $2, $3]; next} ("chr_"$10, $12, $13, "chr_"$14, $16, $17) in a' $file $alnFile > $pslFile
   fi
 done
