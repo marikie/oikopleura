b62003445eeb102b1668c339a60c4f2b5ed5d335
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2023-12-29 13:17:36 +0900

    made a new file: fromPSL2Dotplot_zo.sh
    get zoomed out versions of dotplots

---
+++ analysis/fromPSL2Dotplot_zo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+argNum=1
+
+if [ $# -ne $argNum ]; then
+        echo "You need $argNum arguments." 1>&2
+        echo "- path to the target directory" 1>&2 # $1
+        exit 1
+fi
+
+targetDir=$1
+refAnnoFile="/Users/nakagawamariko/biohazard/data/lancelets/ncbi_dataset/data/GCF_000003815.2/genomic.gff"
+qryAnnoFile="/Users/nakagawamariko/biohazard/data/oikopleura/OKI2018_I69_1.0.gm.gff"
+
+cd $targetDir
+
+for file in $(ls $targetDir); do 
+  # echo "$file"
+  if [ -f "$file" ]; then
+    basename="$(basename "$file" ".psl")"
+    outFile=$basename".out"
+    zoPngFile=$basename"_zo.png"
+    
+    refRange=$(awk -F'\t' '{print $15, $16, $17}' ../OUT/$outFile | sort -u | awk '{printf "-1 %s:%s-%s ", $1, $2, $3} END{print ""}' )
+
+    qryRange=$(awk -F'\t' '{print $10, $11, $12}' ../OUT/$outFile | sort -u | awk '{printf "-2 %s:%s-%s ", $1, $2, $3} END{print ""}')
+
+    last-dotplot  --max-gap2=100 --max-gap1=100 --sort1=3 --strands1=1 --border-color=silver --border-pixels=5 --rot1=v  --rot2=h --labels1=2 --labels2=2 --fontsize=10 -a $refAnnoFile -b $qryAnnoFile $refRange $qryRange $file ../PNG/$zoPngFile 
+  fi
+done
