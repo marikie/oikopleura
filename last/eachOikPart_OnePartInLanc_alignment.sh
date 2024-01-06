49a0a610f94ca9e19e970dd35b1ff4f9c0fb293e
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-01-06 13:20:10 +0900

    added some comments, new formatting

--- last/eachOikPart_OnePartInLanc_alignment.sh
+++ last/eachOikPart_OnePartInLanc_alignment.sh
@@ -2,12 +2,12 @@
 
 argNum=4
 if [ $# -ne $argNum ]; then
-        echo "You need $argNum argument" 1>&2
-        echo "- today's date" 1>&2
-        echo "- path to the lanc_oik_last dir" 1>&2
-        echo "- path to the lancelet's reference fasta file" 1>&2
-        echo "- path to the oik's reference fasta file" 1>&2
-        exit 1
+	echo "You need $argNum argument" 1>&2
+	echo "- today's date" 1>&2
+	echo "- path to the lanc_oik_last dir" 1>&2
+	echo "- path to the lancelet's reference fasta file" 1>&2
+	echo "- path to the oik's reference fasta file" 1>&2
+	exit 1
 fi
 
 DATE=$1
@@ -25,62 +25,81 @@ cd $outDirPath
 # lastdb
 echo "---lastdb"
 if [ ! -d $outDirPath/$dbName ]; then
-        echo "making lastdb"
-        mkdir $dbName
-        cd $dbName
-        lastdb -P8 -uMAM8 -c $dbName $lancFASTA
-        cd ..
+	echo "making lastdb"
+	mkdir $dbName
+	cd $dbName
+	lastdb -P8 -uMAM8 -c $dbName $lancFASTA
+	cd ..
 else
-        echo "$dbName already exists"
+	echo "$dbName already exists"
 fi
-        # -P8: makes it faster by using 8 threads (This has no effect on the results.)
-        # -uMAM8: strives for high sensitivity, but use a lot of memory and run time.
-        # -c: Soft-mask lowercase letters.  This means that, when we compare
-        #     these sequences to some other sequences using lastal, lowercase
-        #     letters will be excluded from initial matches.  This will apply
-        #     to lowercase letters in both sets of sequences.
+# -P8: makes it faster by using 8 threads (This has no effect on the results.)
+# -uMAM8: strives for high sensitivity, but use a lot of memory and run time.
+# -c: Soft-mask lowercase letters.  This means that, when we compare
+#     these sequences to some other sequences using lastal, lowercase
+#     letters will be excluded from initial matches.  This will apply
+#     to lowercase letters in both sets of sequences.
 
 # last-train
 echo "--last-train"
 if [ ! -e $trainFile ]; then
-        echo "doing last-train"
-        last-train -P8 --revsym -D1e7 --sample-number=5000 $dbName/$dbName $oikFASTA > $trainFile
+	echo "doing last-train"
+	last-train -P8 --revsym -D1e7 --sample-number=5000 $dbName/$dbName $oikFASTA >$trainFile
 else
-        echo "$trainFile already exists"
+	echo "$trainFile already exists"
 fi
-        # --revsym: Force the substitution scores to have reverse-complement
-        #           symmetry, e.g. score(A→G) = score(T→C).  This is often
-        #           appropriate, if neither strand is "special".
-        # --sample-number=5000: makes last-train use more samples of genome2, 
-        #                       for fear that most of genome2 lacks similarity to genome1. 
-        # -D1e7: Report alignments that are expected 
-        #        by chance at most once per LENGTH query letters
-        #        The defalt sample-length is 2000, so the total number of query letters
-        #        are 10^7. Thus the expected number of alignments by chance is at most one.
+# --revsym: Force the substitution scores to have reverse-complement
+#           symmetry, e.g. score(A→G) = score(T→C).  This is often
+#           appropriate, if neither strand is "special".
+# --sample-number=5000: makes last-train use more samples of genome2,
+#                       for fear that most of genome2 lacks similarity to genome1.
+# -D1e7: Report alignments that are expected
+#        by chance at most once per LENGTH query letters
+#        The defalt sample-length is 2000, so the total number of query letters
+#        are 10^7. Thus the expected number of alignments by chance is at most one.
 
-# lastal 
+# lastal
 echo "---lastal"
-if [ ! -e $maf ]; then 
-        echo "doing lastal"
-        lastal -P8 -D1e7 -m100 --split-f=MAF+ -p $trainFile $dbName/$dbName $oikFASTA | last-postmask > $maf
+if [ ! -e $maf ]; then
+	echo "doing lastal"
+	lastal -P8 -D1e7 -m100 --split-f=MAF+ -p $trainFile $dbName/$dbName $oikFASTA | last-postmask >$maf
 else
-        echo "$maf already exists"
+	echo "$maf already exists"
 fi
+
+# --split-f=MAF+: has the same effect as --split, and also makes
+#                 it show per-base mismap probabilities: the probability that each
+#                 query (chimp) base should be aligned to a different part of the
+#                 reference (human).
+
+# -m100: Maximum multiplicity for initial matches.  Each initial match is
+#        lengthened until it occurs at most this many times in the reference.
+#        If the reference was split into volumes by lastdb, then lastal
+#        uses one volume at a time.  The maximum multiplicity then applies
+#        to each volume, not the whole reference.  This is why voluming
+#        changes the results. (How to decide the num of multiplicity??)
+
+# The result so far is asymmetric: each part of the chimp genome is
+# aligned to at most one part of the human genome, but not vice-versa.
+# (one-to-many alignment)
+
+# oik query length = 6.4 * 10^7.
+# With -D1e7, the expected number of alignments found by chance is at most 6.4.
 
 # maf-convert sam
 if [ ! -e $sam ]; then
-        echo "converting maf to sam"
-        maf-convert -j1e5 -d sam $maf > $sam
+	echo "converting maf to sam"
+	maf-convert -j1e5 -d sam $maf >$sam
 else
-        echo "$sam already exists"
+	echo "$sam already exists"
 fi
 
 # last-dotplot
 echo "---last-dotplot"
 if [ ! -e $pngFile ]; then
-        echo "making $pngFile"
-        #last-dotplot -a ~/oikdata/lancelets/ncbi_dataset/data/GCF_000003815.2/genomic.gff $maf $pngFile
-        last-dotplot $maf $pngFile
+	echo "making $pngFile"
+	#last-dotplot -a ~/oikdata/lancelets/ncbi_dataset/data/GCF_000003815.2/genomic.gff $maf $pngFile
+	last-dotplot $maf $pngFile
 else
-        echo "$pngFile already exists"
+	echo "$pngFile already exists"
 fi
