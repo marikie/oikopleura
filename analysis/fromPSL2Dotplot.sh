a4724f5a7d8bd24331df68e8aa072ef05b2ce8a8
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-01-29 15:36:09 +0900

    modified
    - to give annotation files as inputs
    - to make .png dotplots with geneIDs

--- analysis/fromPSL2Dotplot.sh
+++ analysis/fromPSL2Dotplot.sh
@@ -2,28 +2,30 @@
 argNum=1
 
 if [ $# -ne $argNum ]; then
-        echo "You need $argNum arguments." 1>&2
-        echo "- path to the target directory" 1>&2 # $1
-        exit 1
+	echo "You need $argNum arguments." 1>&2
+	echo "- path to the target directory" 1>&2          # $1
+	echo "- path to the reference annotation file" 1>&2 # $2
+	echo "- path to the query annotation file" 1>&2     # $3
+	exit 1
 fi
 
 targetDir=$1
-refAnnoFile="/Users/nakagawamariko/biohazard/data/lancelets/ncbi_dataset/data/GCF_000003815.2/genomic.gff"
-qryAnnoFile="/Users/nakagawamariko/biohazard/data/oikopleura/OKI2018_I69_1.0.gm.gff"
+refAnnoFile=$2
+qryAnnoFile=$3
 
 cd $targetDir
 
-for file in $(ls $targetDir); do 
-  # echo "$file"
-  if [ -f "$file" ]; then
-    basename="$(basename "$file" ".psl")"
-    ziPngFile=$basename"_zi.png"
-    # zoPngFile="$(basename)_zo.png"
-    
-    # echo "$pslFile"
-    python ~/biohazard/oikopleura/last/last-dotplot_mariko_1513.py --sort1=3 --strands1=1 --border-color=silver --rot1=v --labels1=2 --labels2=2 --fontsize=10 -a $refAnnoFile -b $qryAnnoFile $file ../PNG/$ziPngFile
+for file in $(ls $targetDir); do
+	# echo "$file"
+	if [ -f "$file" ]; then
+		basename="$(basename "$file" ".psl")"
+		ziPngFile=$basename"_zi.png"
+		ziIDPngFile=$basename"_ziID.png"
+		# zoPngFile="$(basename)_zo.png"
 
+		# echo "$pslFile"
+		python ~/biohazard/oikopleura/last/last-dotplot_mariko_1513.py --sort1=3 --strands1=1 --border-color=silver --rot1=v --labels1=2 --labels2=2 --fontsize=10 -a $refAnnoFile -b $qryAnnoFile $file ../PNG/$ziPngFile
 
-    # last-dotplot  --max-gap2=100 --max-gap1=100 --sort1=3 --strands1=1 --border-color=silver --border-pixels=5 --rot1=v  --rot2=h --labels1=2 --labels2=2 --fontsize=10 -a $refAnnoFile -b $qryAnnoFile  -1 NC_049980.1:19957171-19987773 -1 NC_049982.1:15230685-15257151 -2 PAR:5803360-5809454  /Users/nakagawamariko/biohazard/data/lanc_oik_last/eachOikPart_OnePartInLanc_alignment_20230706_sortedByQuery.maf ./test_anno.png\n\n
-  fi
+		last-dotplot --sort1=3 --strands1=1 --border-color=silver --rot1=v --labels1=2 --labels2=2 --fontsize=10 -a $refAnnoFile -b $qryAnnoFile $file ../PNG/$ziIDPngFile
+	fi
 done
