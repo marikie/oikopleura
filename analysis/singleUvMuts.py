22f03ec237e85c9b8763a6127e038bf61d985f9c
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-02-13 11:03:45 +0900

    modified a bit
    - remove the unnecessary if __name__ == "__main__"
    - modified the structure of mutation dict
    - modified to count no_mutations, deletions, and insertions

--- analysis/singleUvMuts.py
+++ analysis/singleUvMuts.py
@@ -12,53 +12,67 @@ import argparse
 from Util import getAlignmentObjsOneByOne
 import csv
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("alignmentFile", help="an alignment .maf file")
-    parser.add_argument(
-        "outputFilePath",
-        help="a tsv file with the following columns: original base, mutated base, the number of such mutations, mutation type (transition or transversion)",
-    )
-    args = parser.parse_args()
-    alignmentFile = args.alignmentFile
-    outputFilePath = args.outputFilePath
-    alnFileHandle = open(alignmentFile)
+parser = argparse.ArgumentParser()
+parser.add_argument("alignmentFile", help="an alignment .maf file")
+parser.add_argument(
+    "outputFilePath",
+    help="a tsv file with the following columns: original base, mutated base, the number of such mutations, mutation type (transition or transversion)",
+)
+args = parser.parse_args()
+alignmentFile = args.alignmentFile
+outputFilePath = args.outputFilePath
+alnFileHandle = open(alignmentFile)
 
-    mutationDict = {
-        "A": {
-            "C": [0, "transversion"],
-            "G": [0, "transition"],
-            "T": [0, "transversion"],
-        },
-        "C": {
-            "A": [0, "transversion"],
-            "G": [0, "transversion"],
-            "T": [0, "transition"],
-        },
-        "G": {
-            "A": [0, "transversion"],
-            "C": [0, "transversion"],
-            "T": [0, "transversion"],
-        },
-        "T": {
-            "A": [0, "transversion"],
-            "C": [0, "transition"],
-            "G": [0, "transversion"],
-        },
-    }
-    for aln in getAlignmentObjsOneByOne(alnFileHandle):
-        rSeq = aln.gSeq.upper()
-        qSeq = aln.rSeq.upper()
-        assert len(rSeq) == len(qSeq), "rSeq and qSeq have different lengths"
-        for rbase, qbase in zip(rSeq, qSeq):
-            if rbase != "-" and qbase != "-":
-                if rbase != qbase:
-                    mutationDict[rbase][qbase][0] += 1
-    alnFileHandle.close()
+mutationDict = {
+    "A": {
+        "A": {"count": 0, "type": "no_mutation"},
+        "C": {"count": 0, "type": "transversion"},
+        "G": {"count": 0, "type": "transition"},
+        "T": {"count": 0, "type": "transversion"},
+        "-": {"count": 0, "type": "deletion"},
+    },
+    "C": {
+        "A": {"count": 0, "type": "transversion"},
+        "C": {"count": 0, "type": "no_mutation"},
+        "G": {"count": 0, "type": "transversion"},
+        "T": {"count": 0, "type": "transition"},
+        "-": {"count": 0, "type": "deletion"},
+    },
+    "G": {
+        "A": {"count": 0, "type": "transversion"},
+        "C": {"count": 0, "type": "transversion"},
+        "G": {"count": 0, "type": "no_mutation"},
+        "T": {"count": 0, "type": "transversion"},
+        "-": {"count": 0, "type": "deletion"},
+    },
+    "T": {
+        "A": {"count": 0, "type": "transversion"},
+        "C": {"count": 0, "type": "transition"},
+        "G": {"count": 0, "type": "transversion"},
+        "T": {"count": 0, "type": "no_mutation"},
+        "-": {"count": 0, "type": "deletion"},
+    },
+    "-": {
+        "A": {"count": 0, "type": "insertion"},
+        "C": {"count": 0, "type": "insertion"},
+        "G": {"count": 0, "type": "insertion"},
+        "T": {"count": 0, "type": "insertion"},
+    },
+}
 
-    # how to write to a file
-    with open(outputFilePath, "w") as f:
-        writer = csv.writer(f, delimiter="\t", lineterminator="\n")
-        for origBase, mutDict in mutationDict.items():
-            for mutBase, info in mutDict.items():
-                writer.writerow([origBase, mutBase, info[0], info[1]])
+for aln in getAlignmentObjsOneByOne(alnFileHandle):
+    rSeq = aln.gSeq.upper()
+    qSeq = aln.rSeq.upper()
+    assert len(rSeq) == len(qSeq), "rSeq and qSeq have different lengths"
+    for rbase, qbase in zip(rSeq, qSeq):
+        mutationDict[rbase][qbase]["count"] += 1
+alnFileHandle.close()
+
+# how to write to a file
+with open(outputFilePath, "w") as f:
+    writer = csv.writer(f, delimiter="\t", lineterminator="\n")
+    # how to write a header
+    writer.writerow(["refBase", "qryBase", "count", "type"])
+    for origBase, mutDict in mutationDict.items():
+        for mutBase, info in mutDict.items():
+            writer.writerow([origBase, mutBase, info["count"], info["type"]])
