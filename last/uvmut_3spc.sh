e2e91e5a65e555bc68e8cbb4c4351e8d841a5c26
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-03-18 10:33:10 +0900

    modified: set the org1 as the top genome

--- last/uvmut_3spc.sh
+++ last/uvmut_3spc.sh
@@ -6,7 +6,7 @@ lastal --version
 argNum=7
 if [ $# -ne $argNum ]; then
 	echo "You need $argNum arguments" 1>&2
-	echo "You'll get one-to-one alignments of org1-org2 and org2-org3.\n The top genome of each alignment .maf file will be org2." 1>&2
+	echo "You'll get one-to-one alignments of org1-org2 and org1-org3.\n The top genome of each alignment .maf file will be org1." 1>&2
 	echo "- today's date" 1>&2                                           # $1
 	echo "- path to the org1 reference fasta file" 1>&2                  # $2
 	echo "- path to the org2 reference fasta file" 1>&2                  # $3
@@ -26,21 +26,21 @@ org1Name=$5
 org2Name=$6
 org3Name=$7
 outDirPath=$6"/$org1Name_$org2Name_$org3Name"
-o2omaf21="$org2Name""2""$org1Name""_one2one_$DATE.maf"
-o2omaf23="$org2Name""2""$org3Name""_one2one_$DATE.maf"
-o2omaf21_sorted="$org2Name""2""$org1Name""_one2one_$DATE_sorted.maf"
-o2omaf23_sorted="$org2Name""2""$org3Name""_one2one_$DATE_sorted.maf"
+o2omaf12="$org1Name""2""$org2Name""_one2one_$DATE.maf"
+o2omaf13="$org1Name""2""$org3Name""_one2one_$DATE.maf"
+o2omaf12_sorted="$org1Name""2""$org2Name""_one2one_$DATE_sorted.maf"
+o2omaf13_sorted="$org1Name""2""$org3Name""_one2one_$DATE_sorted.maf"
 joinedFile="$org1Name""_""$org2Name""_""$org3Name""_$DATE.maf"
 mutFile="mut_"$(echo $joinedFile | sed -e "s/.maf//")".tsv"
 mut3File="mut3_"$(echo $joinedFile | sed -e "s/.maf//")".tsv"
 
-bash ~/scripts/one2one.sh $DATE $outDirPath $org2FASTA $org1FASTA $org2Name $org1Name
-bash ~/scripts/one2one.sh $DATE $outDirPath $org2FASTA $org3FASTA $org2Name $org3Name
+bash ~/scripts/one2one.sh $DATE $outDirPath $org1FASTA $org2FASTA $org1Name $org2Name
+bash ~/scripts/one2one.sh $DATE $outDirPath $org1FASTA $org3FASTA $org1Name $org3Name
 
 # maf-join the two .maf files
-maf-sort $o2oma21 >$o2omaf21_sorted
-maf-sort $o2oma23 >$o2omaf23_sorted
-maf-join $o2omaf21_sorted $o2omaf23_sorted >$joinedFile
+maf-sort $o2oma12 >$o2omaf12_sorted
+maf-sort $o2oma13 >$o2omaf13_sorted
+maf-join $o2omaf12_sorted $o2omaf13_sorted >$joinedFile
 
 # make a .tsv file about single-base mutations
 echo "making a .tsv file about single-base mutations"
