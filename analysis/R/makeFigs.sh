7a88339c398314053a13d09ab352e1e7dac63f2b
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-07-19 18:09:41 +0900

    .sh script for making graphs for apbjc2024

---
+++ analysis/R/makeFigs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+Rscript sbmut.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batBro.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/batBro.pdf 0
+
+Rscript sbmut.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batSep.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/batSep.pdf 0
+
+Rscript sbmut.R ~/biohazard/data/mytTro_mytEdu_mytGal/mut3_mytTro_mytEdu_mytGal_20240428_mytEdu.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/mytEdu.pdf 0.6
+
+Rscript sbmut.R ~/biohazard/data/mytTro_mytEdu_mytGal/mut3_mytTro_mytEdu_mytGal_20240428_mytGal.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/mytGal.pdf 0.5
+
+Rscript sbmut.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspChe.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/aspChe.pdf 0.6
+
+Rscript sbmut.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspCri.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/aspCri.pdf 0
+
+Rscript sbmut.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvCom.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/ulvCom.pdf 0.3
+
+Rscript sbmut.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvMut.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/ulvMut.pdf 0.7
+
+Rscript sbmut.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walHed.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/walHed.pdf 5
+
+Rscript sbmut.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walIch.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/walIch.pdf 4
+
+Rscript sbmut.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proBov.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/proBov.pdf 4
+
+Rscript sbmut.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proCif.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/proCif.pdf 1.5
+
+Rscript sbmut.R ~/biohazard/data/oikDio_oikAlb_oikVan/mut3_oikDio_oikAlb_oikVan_20240415_oikAlb.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/oikAlb.pdf 4
+
+Rscript sbmut.R ~/biohazard/data/oikDio_oikAlb_oikVan/mut3_oikDio_oikAlb_oikVan_20240415_oikVan.tsv ~/biohazard/scripts/manuscripts/apbjc2024/images/oikVan.pdf 5
