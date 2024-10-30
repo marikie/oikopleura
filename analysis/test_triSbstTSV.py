739328721a419f25f2782ae73224008ff317773e
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-10-30 16:57:59 +0900

    added test_triSbstTSV.py

---
+++ analysis/test_triSbstTSV.py
@@ -0,0 +1,29 @@
+import unittest
+import triSbstTSV as script
+import subprocess
+
+
+class TestTriSbstTSV(unittest.TestCase):
+    def test_main(self):
+        print("test_main")
+
+        # inputs
+        self.in1_maf = open("./test/test_triSbstTSV.maf")
+        self.outPath1 = "./test/result_triSbstTSV_out1.tsv"
+
+        # expected outputs
+        self.out1_tsv = open("./test/test_triSbstTSV_out1.tsv")
+
+        ### test1 ###
+        print("test1")
+        # Run the main function with test data
+        script.main(self.in1_maf, self.outPath1)
+        # Check if the contents of the two files (result and expected) are the same using diff
+        result = subprocess.run(
+            ["diff", self.outPath1, self.out1_tsv], capture_output=True
+        )
+        self.assertEqual(result.returncode, 0, "The files are different")
+
+
+if __name__ == "__main__":
+    unittest.main()
