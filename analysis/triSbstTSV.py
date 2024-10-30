e0f5711e7187124dc8fe23f1bcbc1bcc1ed001aa
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-10-30 16:25:27 +0900

    bug fixed
    - plusStrandStart
    - check if substitution or not (ignore ambiguous cases)

--- analysis/triSbstTSV.py
+++ analysis/triSbstTSV.py
@@ -32,33 +32,58 @@ def bothEdgeBasesSame(g1Tri, g2Tri, g3Tri):
     )
 
 
-def convert2PlusStrand(coord, gLength, gStrand):
+def getPlusStrandStart(coord, gLength, gStrand):
     if gStrand == "+":
         return coord
     else:
-        oppCoord = gLength - coord
-        return oppCoord
+        minusEnd = coord + 1
+        plusStart = gLength - minusEnd
+        return plusStart
+
+
+def isSbst(g1Tri, g2Tri, g3Tri):
+    """
+    If the substitution happened only on genome2, return True.
+    If the substitution happened only on genome3, return True.
+    Otherwise, return False.
+    """
+    assert (g1Tri[0] == g2Tri[0] and g2Tri[0] == g3Tri[0]) and (
+        g1Tri[2] == g2Tri[2] and g2Tri[2] == g3Tri[2]
+    ), "edge bases are not the same (not a mutational signature)"
+
+    middleList = [g1Tri[1], g2Tri[1], g3Tri[1]]
+    minority = ""
+    if len(set(middleList)) == 2:
+        for base in set(middleList):
+            if middleList.count(base) == 1:
+                minority = base
+        if g2Tri[1] == minority or g3Tri[1] == minority:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def write2TSV(aln, i, g1Tri, g2Tri, g3Tri, writer):
-    gStart1 = convert2PlusStrand(aln.gStart1, aln.gLength1, aln.gStrand1)
-    gStart2 = convert2PlusStrand(aln.gStart2, aln.gLength2, aln.gStrand2)
-    gStart3 = convert2PlusStrand(aln.gStart3, aln.gLength3, aln.gStrand3)
+    gStart1 = getPlusStrandStart(aln.gStart1, aln.gLength1, aln.gStrand1)
+    gStart2 = getPlusStrandStart(aln.gStart2, aln.gLength2, aln.gStrand2)
+    gStart3 = getPlusStrandStart(aln.gStart3, aln.gLength3, aln.gStrand3)
     writer.writerow(
         [
             aln.gChr1,
             gStart1 + i,
-            gStart1 + i + 3,
+            gStart1 + i + 1,
             aln.gStrand1,
             g1Tri,
             aln.gChr2,
             gStart2 + i,
-            gStart2 + i + 3,
+            gStart2 + i + 1,
             aln.gStrand2,
             g2Tri,
             aln.gChr3,
             gStart3 + i,
-            gStart3 + i + 3,
+            gStart3 + i + 1,
             aln.gStrand3,
             g3Tri,
         ]
@@ -96,8 +121,11 @@ def main(alnFileHandle, outputFilePath):
             g1Tri = gSeq1[i : i + 3]
             g2Tri = gSeq2[i : i + 3]
             g3Tri = gSeq3[i : i + 3]
+            # if all trinucs are the same, go next
+            if g1Tri == g2Tri == g3Tri:
+                continue
             # if indels are included, go next
-            if not (
+            elif not (
                 all(list(map(lambda b: b in set(["A", "C", "G", "T"]), g1Tri)))
                 and all(list(map(lambda b: b in set(["A", "C", "G", "T"]), g2Tri)))
                 and all(list(map(lambda b: b in set(["A", "C", "G", "T"]), g3Tri)))
@@ -105,11 +133,13 @@ def main(alnFileHandle, outputFilePath):
                 continue
             # if there are no indels,
             # and the edge bases are the same,
-            # count original trinucs as total and count mutations if there is a mutation
-            elif bothEdgeBasesSame(g1Tri, g2Tri, g3Tri):
+            # and can consider it as a substitution
+            # write to TSV
+            elif bothEdgeBasesSame(g1Tri, g2Tri, g3Tri) and isSbst(g1Tri, g2Tri, g3Tri):
                 write2TSV(aln, i, g1Tri, g2Tri, g3Tri, writer)
             # if there are no indels,
             # but edge bases are not all the same,
+            # or it's not a substitution,
             # go next
             else:
                 continue
