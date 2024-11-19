f8fd966883c33b495c4efdc9a0ad2aa5c30d6495
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2024-11-19 15:59:36 +0900

    added makeFigs_oriCount.sh makeFigs_sbstCount.sh

---
+++ analysis/R/makeFigs_oriCount.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+Rscript sbmut_oriCount.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batBro.tsv ~/biohazard/data/gigPla_batSep_batBro/batBro_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batSep.tsv ~/biohazard/data/gigPla_batSep_batBro/batSep_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/mytTro_mytEdu_mytGal/mytTro_mytEdu_mytGal_20240513_mytEdu.tsv ~/biohazard/data/mytTro_mytEdu_mytGal/mytEdu_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/mytTro_mytEdu_mytGal/mytTro_mytEdu_mytGal_20240513_mytGal.tsv ~/biohazard/data/mytTro_mytEdu_mytGal/mytGal_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspChe.tsv ~/biohazard/data/aspCos_aspChe_aspCri/aspChe_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspCri.tsv ~/biohazard/data/aspCos_aspChe_aspCri/aspCri_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvCom.tsv ~/biohazard/data/ulvPro_ulvMut_ulvCom/ulvCom_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvMut.tsv ~/biohazard/data/ulvPro_ulvMut_ulvCom/ulvMut_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walHed.tsv ~/biohazard/data/walMel_walIch_walHed/walHed_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walIch.tsv ~/biohazard/data/walMel_walIch_walHed/walIch_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proBov.tsv ~/biohazard/data/proCut_proCif_proBov/proBov_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proCif.tsv ~/biohazard/data/proCut_proCif_proBov/proCif_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/oikDio_oikAlb_oikVan/oikDio_oikAlb_oikVan_20240513_oikAlb.tsv ~/biohazard/data/oikDio_oikAlb_oikVan/oikAlb_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/oikDio_oikAlb_oikVan/mut3_oikDio_oikAlb_oikVan_20240415_oikVan.tsv ~/biohazard/data/oikDio_oikAlb_oikVan/oikVan_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/ostMed_ostTau_ostLuc/mut3_ostMed_ostTau_ostLuc_20240918_ostTau.tsv ~/biohazard/data/ostMed_ostTau_ostLuc/ostTau_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/ostMed_ostTau_ostLuc/mut3_ostMed_ostTau_ostLuc_20240918_ostLuc.tsv ~/biohazard/data/ostMed_ostTau_ostLuc/ostLuc_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/proCutCh_proCifCh_proBovCh/mut3_proCutCh_proCifCh_proBovCh_20240903_proBovCh.tsv ~/biohazard/data/proCutCh_proCifCh_proBovCh/proBovCh_oriCount.pdf 0
+
+Rscript sbmut_oriCount.R ~/biohazard/data/proCutCh_proCifCh_proBovCh/mut3_proCutCh_proCifCh_proBovCh_20240903_proCifCh.tsv ~/biohazard/data/proCutCh_proCifCh_proBovCh/proCifCh_oriCount.pdf 0
---
+++ analysis/R/makeFigs_sbstCount.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batBro.tsv ~/biohazard/data/gigPla_batSep_batBro/batBro_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/gigPla_batSep_batBro/mut3_gigPla_batSep_batBro_20240610_batSep.tsv ~/biohazard/data/gigPla_batSep_batBro/batSep_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/mytTro_mytEdu_mytGal/mytTro_mytEdu_mytGal_20240513_mytEdu.tsv ~/biohazard/data/mytTro_mytEdu_mytGal/mytEdu_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/mytTro_mytEdu_mytGal/mytTro_mytEdu_mytGal_20240513_mytGal.tsv ~/biohazard/data/mytTro_mytEdu_mytGal/mytGal_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspChe.tsv ~/biohazard/data/aspCos_aspChe_aspCri/aspChe_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/aspCos_aspChe_aspCri/mut3_aspCos_aspChe_aspCri_20240617_aspCri.tsv ~/biohazard/data/aspCos_aspChe_aspCri/aspCri_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvCom.tsv ~/biohazard/data/ulvPro_ulvMut_ulvCom/ulvCom_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/ulvPro_ulvMut_ulvCom/mut3_ulvPro_ulvMut_ulvCom_20240610_ulvMut.tsv ~/biohazard/data/ulvPro_ulvMut_ulvCom/ulvMut_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walHed.tsv ~/biohazard/data/walMel_walIch_walHed/walHed_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/walMel_walIch_walHed/mut3_walMel_walIch_walHed_20240611_walIch.tsv ~/biohazard/data/walMel_walIch_walHed/walIch_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proBov.tsv ~/biohazard/data/proCut_proCif_proBov/proBov_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/proCut_proCif_proBov/mut3_proCut_proCif_proBov_20240423_proCif.tsv ~/biohazard/data/proCut_proCif_proBov/proCif_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/oikDio_oikAlb_oikVan/oikDio_oikAlb_oikVan_20240513_oikAlb.tsv ~/biohazard/data/oikDio_oikAlb_oikVan/oikAlb_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/oikDio_oikAlb_oikVan/mut3_oikDio_oikAlb_oikVan_20240415_oikVan.tsv ~/biohazard/data/oikDio_oikAlb_oikVan/oikVan_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/ostMed_ostTau_ostLuc/mut3_ostMed_ostTau_ostLuc_20240918_ostTau.tsv ~/biohazard/data/ostMed_ostTau_ostLuc/ostTau_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/ostMed_ostTau_ostLuc/mut3_ostMed_ostTau_ostLuc_20240918_ostLuc.tsv ~/biohazard/data/ostMed_ostTau_ostLuc/ostLuc_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/proCutCh_proCifCh_proBovCh/mut3_proCutCh_proCifCh_proBovCh_20240903_proBovCh.tsv ~/biohazard/data/proCutCh_proCifCh_proBovCh/proBovCh_sbstCount.pdf 0
+
+Rscript sbmut_sbstCount.R ~/biohazard/data/proCutCh_proCifCh_proBovCh/mut3_proCutCh_proCifCh_proBovCh_20240903_proCifCh.tsv ~/biohazard/data/proCutCh_proCifCh_proBovCh/proCifCh_sbstCount.pdf 0
