95d86ba3595b8cd9b40ba97ae919d7f6807da01f
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2025-07-07 18:01:26 +0900

    updated to count o_coding and o_non-coding

--- last/count_coding_noncoding.sh
+++ last/count_coding_noncoding.sh
@@ -18,16 +18,26 @@ count_coding_noncoding() {
 	local org1GFF=$1
 	local tsv=$2
 	local bed=$3
-	tsv_annot="${tsv%.tsv}_annot.tsv"
 	org1GFF_CDS="${org1GFF%.gff}_CDS.gff"
 	bed_annot="${bed%.bed}_annot.bed"
+	tsv_tmp="${tsv%.tsv}_tmp.tsv"
+	tsv_annot="${tsv%.tsv}_annot.tsv"
 	awk -F"\t" '$3=="CDS"' $org1GFF > $org1GFF_CDS
 	bedtools intersect -c -header -a $bed -b $org1GFF_CDS | awk -F"\t" 'NR==1{$0=$0"\tcount"}1' > $bed_annot
-	awk -F"\t" 'NR==FNR{if($NF==0)zero[$(NF-1)]++; else nonzero[$(NF-1)]++; next}FNR==1{print $0"\tcoding\tnon-coding"; next}{key=$1; print$0"\t"(nonzero[key]?nonzero[key]:0)"\t"(zero[key]?zero[key]:0)}' $bed_annot $tsv > $tsv_annot
+	# add the count of substitutions in the coding region and non-coding region to the tsv file
+	awk -F"\t" 'NR==FNR{if($(NF-1)=="."){next} if($NF==0)nonCoding[$(NF-1)]++; else coding[$(NF-1)]++; next}FNR==1{print $0"\ts_cds\ts_ncds"; next}{key=$1; print$0"\t"(coding[key]?coding[key]:0)"\t"(nonCoding[key]?nonCoding[key]:0)}' $bed_annot $tsv > $tsv_tmp
+	# add the count of original trinucs in the coding region and non-coding region to the tsv file
+	awk -F"\t" 'NR==FNR{if($NF==0)nonCoding[$(NF-2)]++; else coding[$(NF-2)]++; next}FNR==1{print $0"\to_cds\to_ncds"; next}{key=$2; print$0"\t"(coding[key]?coding[key]:0)"\t"(nonCoding[key]?nonCoding[key]:0)}' $bed_annot $tsv_tmp > $tsv_annot
 
-	# Check if mutNum = coding + non-coding
-	awk -F"\t" 'NR!=1 {if($2 != $4 + $5) {print "Error: mutType " $1 " mutNum(" $2 ")!=coding(" $4 ")+non-coding(" $5 ")"; exit 1}}' $tsv_annot
+	# Check if mutNum = s_coding + s_non-coding
+	awk -F"\t" 'NR!=1 {if($3 != $5 + $6) {print "Error: mutType " $1 " mutNum(" $3 ")!=coding(" $5 ")+non-coding(" $6 ")"; exit 1}}' $tsv_annot
 	# Check the exit status of the awk command
+	if [ $? -ne 0 ]; then
+		echo "An error occurred in the awk command. Stopping the script."
+		exit 1  # Exit the script with a non-zero status
+	fi
+	# Check if totalRootNum = o_coding + o_non-coding
+	awk -F"\t" 'NR!=1 {if($4 != $7 + $8) {print "Error: mutType " $1 " totalRootNum(" $4 ")!=coding(" $7 ")+non-coding(" $8 ")"; exit 1}}' $tsv_annot
 	if [ $? -ne 0 ]; then
 		echo "An error occurred in the awk command. Stopping the script."
 		exit 1  # Exit the script with a non-zero status
