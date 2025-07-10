edd97946016dadbcfe44541632757547f9b460a1
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2025-07-10 11:02:45 +0900

    modified(divide into multiple functions), added more graphs to make

--- analysis/R/sbmut.R
+++ analysis/R/sbmut.R
@@ -1,232 +1,236 @@
 library(stringr)
 library(RColorBrewer)
 library(showtext)
+library(sysfonts)
 
 # MODIFIED THE CODE FROM https://github.com/kartong88/Plot-Mutation-Landscape
 
-generate_plot <- function(file_path, filename = 0, graph_type) {
-  ## Perform analysis for the trinucleotide variants
+generate_plots <- function(tsv_path) {
+  data <- read_and_transform_data(tsv_path)
+  isAnno <- check_annotation_columns(data)
+  trinuc.lab <- generate_trinuc_labels(data)
 
-  ### Check which of the mutation variant is missing and add that to the named vector
-  Letters <- c("A", "T", "C", "G")
-  conversion <- c("C>A", "C>G", "C>T", "T>A", "T>C", "T>G")
-  trinuc.combi <- c()
+  # Extract the path without an extension from tsv_path
+  path_without_extension <- tools::file_path_sans_ext(tsv_path)
+  # Create file paths for the plots
+  normgraph_path <- paste(path_without_extension, "_norm.pdf", sep = "")
+  sbstgraph_path <- paste(path_without_extension, "_sbst.pdf", sep = "")
+  origraph_path <- paste(path_without_extension, "_ori.pdf", sep = "")
 
-  # Get all combinations of the conversions
-  conv.combi <- c()
-  for (i in 1:length(Letters)) {
-    for (j in 1:length(conversion)) {
-      for (k in 1:length(Letters)) {
-        combination <- paste(Letters[i], "[", conversion[j], "]", Letters[k], sep = "")
-        conv.combi <- c(conv.combi, combination)
-      }
-    }
+  print(paste("Creating norm graph...", normgraph_path))
+  create_pdf_plot(normgraph_path, data, trinuc.lab, "norm")
+  print(paste("Creating sbst graph...", sbstgraph_path))
+  create_pdf_plot(sbstgraph_path, data, trinuc.lab, "sbst")
+  print(paste("Creating ori graph...", origraph_path))
+  create_pdf_plot(origraph_path, data, trinuc.lab, "ori")
+
+  # Generate additional plots if annotation data is present
+  if (isAnno) {
+    cdsgraph_path <- paste(path_without_extension, "_norm_cds.pdf", sep = "")
+    noncdsgraph_path <- paste(path_without_extension, "_norm_noncds.pdf", sep = "")
+    sbstcdsgraph_path <- paste(path_without_extension, "_sbst_cds.pdf", sep = "")
+    oricdsgraph_path <- paste(path_without_extension, "_ori_cds.pdf", sep = "")
+    sbstnoncdsgraph_path <- paste(path_without_extension, "_sbst_noncds.pdf", sep = "")
+    orinoncdsgraph_path <- paste(path_without_extension, "_ori_noncds.pdf", sep = "")
+    print(paste("Creating cds graph...", cdsgraph_path))
+    create_pdf_plot(cdsgraph_path, data, trinuc.lab, "cds")
+    print(paste("Creating noncds graph...", noncdsgraph_path))
+    create_pdf_plot(noncdsgraph_path, data, trinuc.lab, "noncds")
+    print(paste("Creating sbst cds graph...", sbstcdsgraph_path))
+    create_pdf_plot(sbstcdsgraph_path, data, trinuc.lab, "sbstcds")
+    print(paste("Creating ori cds graph...", oricdsgraph_path))
+    create_pdf_plot(oricdsgraph_path, data, trinuc.lab, "oricds")
+    print(paste("Creating sbst noncds graph...", sbstnoncdsgraph_path))
+    create_pdf_plot(sbstnoncdsgraph_path, data, trinuc.lab, "sbstnoncds")
+    print(paste("Creating ori noncds graph...", orinoncdsgraph_path))
+    create_pdf_plot(orinoncdsgraph_path, data, trinuc.lab, "orinoncds")
   }
 
+  process_norm_graph(data)
+}
+
+read_and_transform_data <- function(file_path) {
   data <- read.csv(file_path, sep = "\t", header = TRUE)
+  data.trans <- setNames(data.frame(t(data[, -1])), data[, 1])
+  return(data.trans)
+}
 
-  conv.data <- setNames(data.frame(t(data[, -1])), data[, 1])
+check_annotation_columns <- function(data) {
+  required_rows <- c("s_cds", "s_ncds", "o_cds", "o_ncds")
+  return(all(required_rows %in% rownames(data)))
+}
 
-  # Set missing mutations as value of zero
-  conv.data[conv.combi[!(conv.combi %in% names(conv.data))]] <- 0
+generate_trinuc_labels <- function(data) {
+  data.names <- names(data)
+  firstchar <- str_extract(data.names, "^.")
+  lastchar <- str_extract(data.names, ".$")
+  midorichar <- str_extract(str_extract(data.names, "(.)>"), "^.")
+  trinuc.lab <- paste(firstchar, midorichar, lastchar, sep = "")
+  return(trinuc.lab)
+}
 
-  data.conv <- names(conv.data)
-
-  conv.label <- str_extract(data.conv, ".>.")
-  firstchar <- str_extract(data.conv, "^.")
-  lastchar <- str_extract(data.conv, ".$")
-  conv.label.all <- paste(conv.label, "_", firstchar, lastchar, sep = "")
-  conv.label.all.sorted <- order(conv.label.all)
-
-  # Generate the trinucleotide label
-  midrefchar <- str_extract(str_extract(data.conv, "(.)>"), "^.")
-  trinuc.lab <- paste(firstchar, midrefchar, lastchar, sep = "")
-  trinuc.lab.sorted <- trinuc.lab[conv.label.all.sorted]
-
-  # Labels information for subsequent barplot
-  # colour_array <- brewer.pal(6, "Set2")
-  colour_array <- brewer.pal(6, "Dark2")
-  # colour_array<-c("red","blue","green","yellow", "purple", "black")
-  text_array <- c("C > A", "C > G", "C > T", "T > A", "T > C", "T > G")
-
-  # Convert the data to percentage
-  ## Calculate the new row and add it
-  ## Add Mutation Percentage
-  mutation_percentage <- as.numeric(conv.data["mutNum", ] / conv.data["totalRootNum", ]) * 100
-  conv.data <- rbind(conv.data, MutationPercentage = mutation_percentage)
-
+extract_values <- function(data, graph_type) {
   if (graph_type == "norm") {
-    conv.data.norm <- as.numeric(conv.data["mutNum", ] / conv.data["totalRootNum", ] * 100)
+    return(as.numeric(as.numeric(data["mutNum", ]) / as.numeric(data["totalRootNum", ]) * 100))
   } else if (graph_type == "sbst") {
-    conv.data.norm <- as.numeric(conv.data["mutNum", ])
+    return(as.numeric(data["mutNum", ]))
   } else if (graph_type == "ori") {
-    conv.data.norm <- as.numeric(conv.data["totalRootNum", ])
+    return(as.numeric(data["totalRootNum", ]))
+  } else if (graph_type == "cds") {
+    return(as.numeric(as.numeric(data["s_cds", ]) / as.numeric(data["o_cds", ]) * 100))
+  } else if (graph_type == "noncds") {
+    return(as.numeric(as.numeric(data["s_ncds", ]) / as.numeric(data["o_ncds", ]) * 100))
+  } else if (graph_type == "sbstcds") {
+    return(as.numeric(data["s_cds", ]))
+  } else if (graph_type == "sbstnoncds") {
+    return(as.numeric(data["s_ncds", ]))
+  } else if (graph_type == "oricds") {
+    return(as.numeric(data["o_cds", ]))
+  } else if (graph_type == "orinoncds") {
+    return(as.numeric(data["o_ncds", ]))
   } else {
     stop("Invalid graph type")
   }
-  # Compute y-axis maximum with conditional rounding when ceiling(raw_max) == 1
-  raw_max <- max(na.omit(conv.data.norm[conv.label.all.sorted]))
+}
+
+compute_y_axis_max <- function(conv.data.norm) {
+  raw_max <- max(na.omit(conv.data.norm))
   if (ceiling(raw_max) == 1) {
     if (raw_max < 0.1) {
-      pct_yaxs_max <- round(raw_max, 2)
+      return(round(raw_max, 2))
     } else {
-      pct_yaxs_max <- round(raw_max, 1)
+      return(round(raw_max, 1))
     }
   } else {
-    pct_yaxs_max <- ceiling(raw_max)
-  }
-
-  # Transform each column name according to the specified pattern
-  transformed_names <- sapply(colnames(conv.data), function(name) {
-    # Split the name into a character vector
-    char_vector <- strsplit(name, "")[[1]]
-    # Check if the length of char_vector is less than 7
-    if (length(char_vector) < 7) {
-      return(name) # Return original name if it's too short
-    }
-    # Extract characters based on the specified positions
-    original_chars <- char_vector[c(1, 3, 7)]
-    final_chars <- char_vector[c(1, 5, 7)]
-    # Combine into the new format
-    result <- paste(paste(original_chars, collapse = ""), " → ", paste(final_chars, collapse = ""), sep = "")
-    return(result)
-  })
-  # Update the column names of the data frame
-  colnames(conv.data) <- transformed_names
-
-  # Sorting columns directly based on 'MutationPercentage' row values
-  # Get the order of indices based on 'MutationPercentage' values
-  col_order <- order(as.numeric(conv.data["MutationPercentage", ]), decreasing = TRUE)
-  # Reorder the columns based on sorted indices
-  conv.data.sorted <- conv.data[, col_order]
-
-
-  ### print Top 10 and Worst 10
-  # Print the sorted data frame
-  print("Data sorted by Mutation Percentage:")
-  print(conv.data.sorted)
-
-  cat("\n\nTop 10:\n")
-  print(conv.data.sorted[, 1:10])
-  cat("\n\nWorst 10:\n")
-  print(conv.data.sorted[, (ncol(conv.data.sorted) - 9):ncol(conv.data.sorted)])
-  # Get the reverse order of indices based on 'MutationPercentage' values
-  col_order_rev <- order(as.numeric(conv.data["MutationPercentage", ]))
-  # Reorder the columns based on sorted indices
-  conv.data.revsorted <- conv.data[, col_order_rev]
-  cat("\n\nWorst 10 (rev):\n")
-  # Reverse-sort the data frame 'conv.data.sorted' based on the third row
-  print(conv.data.revsorted[, 1:10])
-
-
-  ### Barplot for Trinucleotide Mutation rate (Percentage)
-  # Make the PDF plot of the graph
-  if (!filename == 0) {
-    pdf(filename, width = 30, height = 8)
-    # default: c(5, 4, 4, 2) + 0.1
-    # font_add_google("IBM Plex Mono", "mn", 500)
-    font_add_google("Courier Prime", "mn", 700)
-    font_add_google("Roboto", "os")
-    showtext_auto()
-    par(family = "mn", mar = c(7.5, 6, 4, 1), cex.axis = 2) # Increase the size of y-axis numbers # Increase the bottom, left, top, and right margins
-    bar_positions <- barplot(
-      axes = FALSE,
-      family = "os",
-      # main = orgName, # graph title
-      as.numeric(conv.data.norm[conv.label.all.sorted]),
-      col = rep(colour_array, each = 16),
-      cex.names = 0.7, # default size (manually write over later)
-      las = 3, # rotate labels by 90 degrees (but not needed?)
-      names.arg = rep("", length(trinuc.lab.sorted)), # empty labels for now (not needed?)
-      # names.arg=trinuc.lab.sorted,
-      ylim = c(0, pct_yaxs_max + 0.15 * pct_yaxs_max),
-      space = 0
-    ) # Increase space between bars
-
-    # manually add y axis
-    axis(side = 2, line = -3.5)
-
-    # manually add labels
-    # text(x=bar_positions, y=par("usr")[3] - 0.05 * (par("usr")[4] - par("usr")[3]),
-    #     labels=trinuc.lab.sorted, srt=90, adj=1, xpd=TRUE, cex=2) # modify size by cex, rotate by srt
-    for (i in seq_along(trinuc.lab.sorted)) {
-      label <- trinuc.lab.sorted[i]
-      x_pos <- bar_positions[i]
-      base_y <- par("usr")[3] - 0.03 * (par("usr")[4] - par("usr")[3])
-      if (i %% 16 != 0) {
-        coln <- i %/% 16 + 1
-      } else {
-        coln <- i %/% 16
-      }
-
-      # print("strwidth:")
-      # print(strwidth(label))
-      # print("strheight:")
-      # print(strheight(label))
-      # print(label)
-
-      # Last letter (slightly under the x axis)
-      text(
-        family = "mn", x_pos, base_y, substr(label, 3, 3),
-        xpd = TRUE, cex = 2, srt = 90
-      )
-
-      # Middle letter (colored red, slightly above the first letter)
-      text(
-        family = "mn", x_pos, base_y - strwidth(label) * 0.03 * (par("usr")[4] - par("usr")[3]), substr(label, 2, 2),
-        xpd = TRUE, cex = 2, col = colour_array[coln], srt = 90
-      )
-
-      # First letter (at the bottom)
-      text(
-        family = "mn", x_pos, base_y - strwidth(label) * 0.06 * (par("usr")[4] - par("usr")[3]), substr(label, 1, 1),
-        xpd = TRUE, cex = 2, srt = 90
-      )
-    }
-
-    # add xlab and ylab
-    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5) # modify size by cex
-    mtext(family = "os", "#Subs/#OrigTrinucs (%)", side = 2, line = 0.5, cex = 2.5) # cexでサイズ調整
-
-    # Calculate the width of the bars
-    bar_widths <- diff(bar_positions)
-    bar_width <- mean(bar_widths)
-    # Print the bar width for debugging
-    # print(paste("Bar width: ", bar_width))
-
-    total_size_per_group <- bar_width * 16
-
-    # print("pct_yaxs_max: ")
-    # print(pct_yaxs_max)
-    for (i in 1:6) {
-      # Size of 1.2 per bar.
-      # Total size of 19.2 for 16 barplots
-      left <- (i - 1) * total_size_per_group + 0.2 # to create a bit of white space
-      right <- i * total_size_per_group - 0.2
-      label_mid <- total_size_per_group / 2 + (i - 1) * total_size_per_group
-
-      # rect(left,5.1,right,5.2, col=colour_array[i], border=NA)
-      # rect(left,0.90*pct_yaxs_max,right,0.93*pct_yaxs_max, col=colour_array[i], border=NA)
-      rect(left, pct_yaxs_max + 0.05 * pct_yaxs_max, right, pct_yaxs_max + 0.08 * pct_yaxs_max, col = colour_array[i], border = NA)
-      text(family = "mn", x = label_mid, y = pct_yaxs_max + 0.12 * pct_yaxs_max, labels = text_array[i], cex = 2.5) # increase label size with cex
-    }
-
-    dev.off()
+    return(ceiling(raw_max))
   }
 }
 
+create_pdf_plot <- function(filename, data, trinuc.lab, graph_type) {
+  conv.data.norm <- extract_values(data, graph_type)
+  pct_yaxs_max <- compute_y_axis_max(conv.data.norm)
+  color_array <- brewer.pal(6, "Dark2")
+  pdf(filename, width = 30, height = 8)
+  font_add_google("Courier Prime", "mn", 700)
+  font_add_google("Roboto", "os")
+  showtext_auto()
+  par(family = "mn", mar = c(7.5, 6, 4, 1), cex.axis = 2)
+  bar_positions <- barplot(
+    axes = FALSE,
+    family = "os",
+    as.numeric(conv.data.norm),
+    col = rep(color_array, each = 16),
+    cex.names = 0.7,
+    las = 3,
+    ylim = c(0, pct_yaxs_max + 0.15 * pct_yaxs_max),
+    space = 0
+  )
+  # manually add y axis
+  axis(side = 2, line = -3.5)
+  add_labels(trinuc.lab, bar_positions)
+  add_axis_names(graph_type)
+  if (graph_type != "ori" && graph_type != "oricds" && graph_type != "orinoncds") {
+    add_colored_rectangles(bar_positions, pct_yaxs_max, color_array)
+  }
+  dev.off()
+}
 
+add_labels <- function(trinuc.lab, bar_positions) {
+  for (i in seq_along(trinuc.lab)) {
+    label <- trinuc.lab[i]
+    x_pos <- bar_positions[i]
+    base_y <- par("usr")[3] - 0.03 * (par("usr")[4] - par("usr")[3])
+    if (i %% 16 != 0) {
+      coln <- i %/% 16 + 1
+    } else {
+      coln <- i %/% 16
+    }
+    text(family = "mn", x_pos, base_y, substr(label, 3, 3), xpd = TRUE, cex = 2, srt = 90)
+    text(family = "mn", x_pos, base_y - strwidth(label) * 0.03 * (par("usr")[4] - par("usr")[3]), substr(label, 2, 2), xpd = TRUE, cex = 2, col = brewer.pal(6, "Dark2")[coln], srt = 90)
+    text(family = "mn", x_pos, base_y - strwidth(label) * 0.06 * (par("usr")[4] - par("usr")[3]), substr(label, 1, 1), xpd = TRUE, cex = 2, srt = 90)
+  }
+}
+
+add_axis_names <- function(graph_type) {
+  if (graph_type == "norm") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#Sbst/#OrigTrinucs (%)", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "sbst") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "# Substitutions", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "ori") {
+    mtext(family = "os", "Trinucleotide Patterns", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "# Original Trinucleotides", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "cds") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#SbstInCDS/#OrigTrinucsInCDS (%)", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "noncds") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#SbstInNonCDS/#OrigInNonCDS (%)", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "sbstcds") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#SbstInCDS", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "sbstnoncds") {
+    mtext(family = "os", "Original Trinucleotides", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#SbstInNonCDS", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "oricds") {
+    mtext(family = "os", "Trinucleotide Patterns", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#OrigInCDS", side = 2, line = 0.5, cex = 2.5)
+  } else if (graph_type == "orinoncds") {
+    mtext(family = "os", "Trinucleotide Patterns", side = 1, line = 5.5, cex = 2.5)
+    mtext(family = "os", "#OrigInNonCDS", side = 2, line = 0.5, cex = 2.5)
+  }
+}
+
+add_colored_rectangles <- function(bar_positions, pct_yaxs_max, color_array) {
+  total_size_per_group <- mean(diff(bar_positions)) * 16
+  for (i in 1:6) {
+    left <- (i - 1) * total_size_per_group + 0.2
+    right <- i * total_size_per_group - 0.2
+    label_mid <- total_size_per_group / 2 + (i - 1) * total_size_per_group
+    rect(left, pct_yaxs_max + 0.05 * pct_yaxs_max, right, pct_yaxs_max + 0.08 * pct_yaxs_max, col = color_array[i], border = NA)
+    text(family = "mn", x = label_mid, y = pct_yaxs_max + 0.12 * pct_yaxs_max, labels = c("C > A", "C > G", "C > T", "T > A", "T > C", "T > G")[i], cex = 2.5)
+  }
+}
+
+process_norm_graph <- function(data) {
+  mutation_percentage <- as.numeric(as.numeric(data["mutNum", ]) / as.numeric(data["totalRootNum", ]) * 100)
+  data <- rbind(data, MutationPercentage = mutation_percentage)
+
+  # Transform column names
+  transformed_names <- sapply(colnames(data), function(name) {
+    char_vector <- strsplit(name, "")[[1]]
+    if (length(char_vector) < 7) {
+      return(name)
+    }
+    original_chars <- char_vector[c(1, 3, 7)]
+    final_chars <- char_vector[c(1, 5, 7)]
+    result <- paste(paste(original_chars, collapse = ""), " → ", paste(final_chars, collapse = ""), sep = "")
+    return(result)
+  })
+  colnames(data) <- transformed_names
+
+  # Sort data
+  col_order <- order(as.numeric(data["MutationPercentage", ]), decreasing = TRUE)
+  data.sorted <- data[, col_order]
+  print("Data sorted by Mutation Percentage:")
+  print(data.sorted)
+  cat("\n\nTop 10:\n")
+  print(data.sorted[, 1:10])
+  cat("\n\nWorst 10:\n")
+  print(data.sorted[, (ncol(data.sorted) - 9):ncol(data.sorted)])
+
+  # Reverse sort
+  col_order_rev <- order(as.numeric(data["MutationPercentage", ]))
+  data.revsorted <- data[, col_order_rev]
+  cat("\n\nWorst 10 (rev):\n")
+  print(data.revsorted[, 1:10])
+}
+
+
+# Access the arguments
 args <- commandArgs(trailingOnly = TRUE)
 
 # Access the arguments
 tsv_path <- args[1] # File path for the input data, .tsv file
-# Extract the path without an extension from tsv_path
-path_without_extension <- tools::file_path_sans_ext(tsv_path)
-normgraph_path <- paste(path_without_extension, ".pdf", sep = "")
-sbstgraph_path <- paste(path_without_extension, "_sbstCount.pdf", sep = "")
-origraph_path <- paste(path_without_extension, "_oriCount.pdf", sep = "")
-
-# generate_plot(tsv_path, filename = graph_path, as.numeric(ymax_plus), orgName)
-generate_plot(tsv_path, filename = normgraph_path, graph_type = "norm")
-generate_plot(tsv_path, filename = sbstgraph_path, graph_type = "sbst")
-generate_plot(tsv_path, filename = origraph_path, graph_type = "ori")
+generate_plots(tsv_path)
