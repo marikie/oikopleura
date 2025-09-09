c68439900e211d878af821c1160dec7e2421fd88
Mariko NAKAGAWA <mariko.n.0424@gmail.com>
2025-09-09 12:36:42 +0900

    still need a big modification, currently mathmatically wrong

--- analysis/R/logRatioPlot.R
+++ analysis/R/logRatioPlot.R
@@ -53,9 +53,13 @@ create_pdf <- function(graph_path, data, value_col = "logRatio") {
     mutate(pos = pos_in_group + offset)
 
   # Stats
-  mean_val <- mean(data[[value_col]], na.rm = TRUE)
-  sd_val <- sd(data[[value_col]], na.rm = TRUE)
   value_sym <- rlang::sym(value_col)
+  mean_val <- mean(data[[value_col]], na.rm = TRUE)
+  # mean_val <- data %>%
+  #   filter(substr(oriType, 2, 2) == "C") %>%
+  #   pull(!!value_sym) %>%
+  #   mean(na.rm = TRUE)
+  sd_val <- sd(data[[value_col]], na.rm = TRUE)
 
   # X labels: oriType in the arranged order
   x_breaks <- data$pos
@@ -161,7 +165,8 @@ create_pdf <- function(graph_path, data, value_col = "logRatio") {
     geom_hline(yintercept = mean_val + 2 * sd_val, linetype = "dashed", color = line_col_2sd) +
     geom_hline(yintercept = mean_val - 2 * sd_val, linetype = "dashed", color = line_col_2sd) +
     scale_x_continuous(breaks = x_breaks, labels = x_labels, expand = c(0, 0), minor_breaks = NULL) +
-    labs(x = "Ancestral Trinucleotides", y = "Log2{(#Observed sbst/#Observed ans)\n/ (sum of all sbst/sum of all ans)}") +
+    # labs(x = "Ancestral Trinucleotides", y = "Log2{(#Observed sbst/#Observed anc)\n/ ((sum of all sbst/sum of all anc)/3)}") +
+    labs(x = "Ancestral Trinucleotides", y = "Log2{(#Observed sbst/#Observed anc)\n/ (sum of all sbst/sum of all anc)}") +
     theme_minimal(base_size = 12) +
     ggplot2::theme(
       axis.text.x = element_blank(),
@@ -205,15 +210,76 @@ add_logRatio_ncds <- function(data) {
   all_sbst_ncds_sum <- data %>%
     select(s_ncds) %>%
     sum()
+  sbst_ncds_c_sum <- data %>%
+    filter(substr(oriType, 2, 2) == "C") %>%
+    select(s_ncds) %>%
+    sum()
+  sbst_ncds_t_sum <- data %>%
+    filter(substr(oriType, 2, 2) == "T") %>%
+    select(s_ncds) %>%
+    sum()
   all_ori_ncds_sum <- data %>%
     select(oriType, o_ncds) %>%
     unique() %>%
     pull(o_ncds) %>%
     sum()
+  # Get the sum of o_ncds where the middle letter of oriType is "C"
+  ori_ncds_c_sum <- data %>%
+    filter(substr(oriType, 2, 2) == "C") %>%
+    select(oriType, o_ncds) %>%
+    unique() %>%
+    pull(o_ncds) %>%
+    sum()
+  ori_ncds_t_sum <- data %>%
+    filter(substr(oriType, 2, 2) == "T") %>%
+    select(oriType, o_ncds) %>%
+    unique() %>%
+    pull(o_ncds) %>%
+    sum()
+
   allsbst_ncds_over_allori_ncds <- all_sbst_ncds_sum / all_ori_ncds_sum
+  sbst_ncds_c_over_ori_ncds_c <- sbst_ncds_c_sum / ori_ncds_c_sum
+  sbst_ncds_t_over_ori_ncds_t <- sbst_ncds_t_sum / ori_ncds_t_sum
+  print(paste("sbst_ncds_c_over_ori_ncds_c: ", sbst_ncds_c_over_ori_ncds_c))
+  print(paste("sbst_ncds_t_over_ori_ncds_t: ", sbst_ncds_t_over_ori_ncds_t))
+  print(paste("allsbst_ncds_over_allori_ncds: ", allsbst_ncds_over_allori_ncds))
+  print(paste("sbst_ncds_c_over_ori_ncds_c/3: ", sbst_ncds_c_over_ori_ncds_c / 3))
+  print(paste("sbst_ncds_t_over_ori_ncds_t/3: ", sbst_ncds_t_over_ori_ncds_t / 3))
+  print(paste("allsbst_ncds_over_allori_ncds/3: ", allsbst_ncds_over_allori_ncds / 3))
+
+  print(paste("mean of obs_snc_over_onc: ", data %>% pull(obs_snc_over_onc) %>% mean(na.rm = TRUE)))
+  print(paste("mean of obs_snc_over_onc_c: ", data %>% filter(substr(oriType, 2, 2) == "C") %>% pull(obs_snc_over_onc) %>% mean(na.rm = TRUE)))
+  print(paste("mean of obs_snc_over_onc_t: ", data %>% filter(substr(oriType, 2, 2) == "T") %>% pull(obs_snc_over_onc) %>% mean(na.rm = TRUE)))
+
+  mean_obs_snc_over_onc <- data %>%
+    pull(obs_snc_over_onc) %>%
+    mean(na.rm = TRUE)
+  # Add logRatio_ncds column based on the middle base of oriType
+  # data <- data %>%
+  #   mutate(
+  #     logRatio_ncds = case_when(
+  #       substr(oriType, 2, 2) == "C" ~ log2(obs_snc_over_onc / (sbst_ncds_c_over_ori_ncds_c / 3)),
+  #       substr(oriType, 2, 2) == "T" ~ log2(obs_snc_over_onc / (sbst_ncds_t_over_ori_ncds_t / 3)),
+  #       TRUE ~ NA_real_
+  #     )
+  #   )
 
   data <- data %>%
-    mutate(logRatio_ncds = log2(obs_snc_over_onc / allsbst_ncds_over_allori_ncds))
+    mutate(logRatio_ncds = log2(obs_snc_over_onc / mean_obs_snc_over_onc))
+  # mutate(logRatio_ncds = log2(obs_snc_over_onc / allsbst_ncds_over_allori_ncds))
+
+  print(paste("mean of logRatio_ncds: ", data %>% pull(logRatio_ncds) %>% mean(na.rm = TRUE)))
+  # logRatio_ncds_c_mean <- data %>%
+  #   filter(substr(oriType, 2, 2) == "C") %>%
+  #   pull(logRatio_ncds) %>%
+  #   mean(na.rm = TRUE)
+  # logRatio_ncds_t_mean <- data %>%
+  #   filter(substr(oriType, 2, 2) == "T") %>%
+  #   pull(logRatio_ncds) %>%
+  #   mean(na.rm = TRUE)
+  # print(paste("logRatio_ncds_c_mean: ", logRatio_ncds_c_mean))
+  # print(paste("logRatio_ncds_t_mean: ", logRatio_ncds_t_mean))
+
   return(data)
 }
 
